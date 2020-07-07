3e42047d97b3acac1fdc115ed7502b4b3ac6208c
Сергей Серый <kisa99.sk@gmail.com>
2020-07-07 10:48:00 +0300

    Student cotroller added

--- CharlieBackend.Api/Controllers/StudentsController.cs
+++ CharlieBackend.Api/Controllers/StudentsController.cs
@@ -1,4 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CharlieBackend.Business.Services.Interfaces;
+using CharlieBackend.Core.Models.Student;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CharlieBackend.Api.Controllers
 {
@@ -6,5 +13,39 @@ namespace CharlieBackend.Api.Controllers
     [ApiController]
     public class StudentsController : ControllerBase
     {
+
+        private readonly IStudentService _studentService;
+        private readonly IAccountService _accountService;
+
+        public StudentsController(IStudentService studentService, IAccountService accountService)
+        {
+            _studentService = studentService;
+            _accountService = accountService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> PostStudent(CreateStudentModel studentModel)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            var isEmailTaken = await _accountService.IsEmailTakenAsync(studentModel.Email);
+            if (isEmailTaken) return StatusCode(409, "Account already exists!");
+
+            var createdStudentModel = await _studentService.CreateStudentAsync(studentModel);
+            if (createdStudentModel == null) return StatusCode(422, "Invalid courses.");
+
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<StudentModel>>> GetAllStudents()
+        {
+            try
+            {
+                var studentsModels = await _studentService.GetAllStudentsAsync();
+                return Ok(studentsModels);
+            }
+            catch { return StatusCode(500); }
+        }
     }
 }
