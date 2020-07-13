d00d09a104597152bbc9b1911b195f70904c918f
Egor Yudin <eydnep2000@gmail.com>
2020-07-13 17:29:58 +0300

    Port changed to 5000.

--- CharlieBackend.Api/Program.cs
+++ CharlieBackend.Api/Program.cs
@@ -14,8 +14,7 @@ namespace CharlieBackend.Api
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>()
-                    .UseUrls("http://localhost:4000");
+                    webBuilder.UseStartup<Startup>();
                 });
     }
 }
