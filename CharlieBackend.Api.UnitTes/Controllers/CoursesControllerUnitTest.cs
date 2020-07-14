11be22a4f2207e6067ca6b89f9b39cfc257eea2a
Сергей Серый <kisa99.sk@gmail.com>
2020-07-14 13:04:37 +0300

    Added unitTest for Courses(Get)

--- CharlieBackend.Api.UnitTes/Controllers/CoursesControllerUnitTest.cs
+++ CharlieBackend.Api.UnitTes/Controllers/CoursesControllerUnitTest.cs
@@ -5,60 +5,66 @@ using Moq;
 using CharlieBackend.Core.Models.Course;
 using System.Threading.Tasks;
 using Xunit;
-using System.Linq;
-using Microsoft.VisualBasic;
-using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CharlieBackend.Api.Controllers.Tests
 {
 
 	public class CoursesControllerUnitTest
 	{
+
 		//[Fact]
 		//public void CoursesControllerTest()
 		//{
-
+		//}
+		//[Fact]
+		//public void PostCourseTest()
+		//{
+		//	var mock = new Mock<ICourseService>();
+		//	mock.Setup(repo => repo.GetAllCoursesAsync()).Returns(courses);
+		//	var newCourse = new CreateCourseModel { Id = 232, Name = "Test323" };
+		//	CoursesController controller = new CoursesController(mock.Object);
+		//	var result = controller.PostCourse(newCourse);
 		//}
 
 		[Fact]
-		public void PostCourseTest()
-		{
-
-			var mock = new Mock<ICourseService>();
-			mock.Setup(repo => repo.GetAllCoursesAsync()).Returns(courses);
-			var newCourse = new CreateCourseModel { Id = 232, Name = "Test323" };
-			CoursesController controller = new CoursesController(mock.Object);
-
-			var result = controller.PostCourse(newCourse);
-
-
-
-
-		}
-
-		[Fact]
-		public void GetAllCoursesTest()
+		public async Task GetAllCoursesTestAsync()
 		{
 			var mock = new Mock<ICourseService>();
 			mock.Setup(repo => repo.GetAllCoursesAsync()).Returns(courses);
+
 			CoursesController controller = new CoursesController(mock.Object);
 
 			var GetResult = controller.GetAllCourses();
 
-			Assert.NotNull(GetResult);
+			var a = GetResult.Result.Result as ObjectResult;
+			var temp = a.Value as List<CourseModel>;
+
+			var e = await courses();
+
+			Assert.Equal(e.Count,temp.Count);
 		}
 
-		//[Fact]
+		public async Task<List<CourseModel>> courses()
+		{
+			List<CourseModel> coursesL = new List<CourseModel>();
+			coursesL.Add(new CourseModel { Id = 12, Name = "Charli" });
+			coursesL.Add(new CourseModel { Id = 13, Name = "Alfa" });
+			coursesL.Add(new CourseModel { Id = 14, Name = "Omega" });
+
+			return coursesL;
+		}	
+
+
+
+//[Fact]
 		//public void PutCourseTest()
 		//{
-
 		//}
 
-		Task<List<CourseModel>> courses = new Task<List<CourseModel>>(() => new List<CourseModel>
-			{
-				new CourseModel{ Id = 1, Name = "Test1"},
-				new CourseModel{ Id = 2, Name = "Test2"},
-				new CourseModel{ Id = 3, Name = "Test3"}
-			});
+		////var s = tr();
+		////s.Start();
+		////var PressF = s.Result;
+		////Assert.Equal("3", PressF );
 	}
 }
