9381465f30e8cdfc936ee81c6b82027b19e4b2f5
Egor Yudin <eydnep2000@gmail.com>
2020-07-14 15:01:16 +0300

    CORS added.

--- CharlieBackend.Api/Startup.cs
+++ CharlieBackend.Api/Startup.cs
@@ -82,6 +82,13 @@ namespace CharlieBackend.Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(builder =>
+            {
+                builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+            });
             // Enable middleware to serve generated Swagger as a JSON endpoint.
             app.UseSwagger(c =>
             {
