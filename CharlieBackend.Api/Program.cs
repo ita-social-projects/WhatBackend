88b26e2f7d2165b2b66378939b89b04f1e090856
SerjKocherga <serj911kocherga@gmail.com>
2020-10-21 16:01:20 +0300

    added serilog and configured

--- CharlieBackend.Api/Program.cs
+++ CharlieBackend.Api/Program.cs
@@ -1,5 +1,13 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using NuGet.Frameworks;
+using Serilog;
+using Serilog.Events;
+using Serilog.Formatting.Compact;
+using Serilog.Sinks.File;
+using Serilog.Settings.Configuration;
+using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace CharlieBackend.Api
 {
@@ -7,11 +15,37 @@ namespace CharlieBackend.Api
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var configuration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+
+            //Configuration for Serilog
+            Log.Logger = new LoggerConfiguration()
+                    .ReadFrom.Configuration(configuration, sectionName: "Logging")
+                    .Enrich.FromLogContext()
+                    .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day, 
+                                   retainedFileCountLimit: null, fileSizeLimitBytes: null)
+                    .CreateLogger();
+
+            try
+            {
+                Log.Information("Application has started...");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (System.Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog() //Serilog added
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
