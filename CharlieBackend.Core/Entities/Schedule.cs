b2196a7713e5b72121f2f18b95eda25c2ae0fbcc
Vladyslav Volkovskyi <volkovskiy4@gmail.com>
2020-10-30 01:40:15 +0200

    Create shedule entity

---
+++ CharlieBackend.Core/Entities/Schedule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CharlieBackend.Core.Entities
+{
+    class Schedule : BaseEntity
+    {
+        public DateTime LessonStart { get; set; }
+        public DateTime LessonEnd { get; set; }
+        public virtual ICollection<StudentGroup> StudentGroups { get; set; }
+        public virtual ICollection<Mentor> Mentors { get; set; }
+        public virtual ICollection<Student> Students { get; set; }
+    }
+}
