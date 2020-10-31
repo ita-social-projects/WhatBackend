1bc0a1f1bcb4f7f12f7b35278afd5beea1be4284
Vladyslav Volkovskyi <volkovskiy4@gmail.com>
2020-11-01 00:39:52 +0200

    Add availability of repeating in shedule entity

--- CharlieBackend.Core/Entities/Schedule.cs
+++ CharlieBackend.Core/Entities/Schedule.cs
@@ -1,17 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel.DataAnnotations;   
 
 namespace CharlieBackend.Core.Entities
 {
     public partial class Schedule : BaseEntity
     {
-        public DateTime LessonStart { get; set; }
+        public TimeSpan LessonStart { get; set; }
 
-        public DateTime LessonEnd { get; set; }
-        
+        public TimeSpan LessonEnd { get; set; }
+
         public long? StudentGroupId { get; set; }
 
         public virtual StudentGroup StudentGroup { get; set; }
+
+        public virtual RepeatRate RepeatRate { get; set; }
+
+        [Range(1, 31)]   
+        public uint? DayNumber { get; set; }
+    }
+
+    public enum RepeatRate{
+        Never,
+        Daily,
+        Weekly,
+        Monthly
     }
 }
