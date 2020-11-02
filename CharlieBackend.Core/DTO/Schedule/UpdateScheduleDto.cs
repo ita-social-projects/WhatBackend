7360d1140a1591f9b211b21a638f01f9f7f6b146
Vladyslav Volkovskyi <volkovskiy4@gmail.com>
2020-11-02 20:23:31 +0200

    added json property names to dto

--- CharlieBackend.Core/DTO/Schedule/UpdateScheduleDto.cs
+++ CharlieBackend.Core/DTO/Schedule/UpdateScheduleDto.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using CharlieBackend.Core.Entities;
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace CharlieBackend.Core.DTO.Schedule
 {
@@ -11,18 +12,22 @@ namespace CharlieBackend.Core.DTO.Schedule
         public long Id { get; set; }
 
         [Required]
+        [JsonPropertyName("start_time")]
         [RegularExpression(@"((([0-1][0-9])|(2[0-3]))(:[0-5][0-9])(:[0-5][0-9])?)", 
             ErrorMessage = "Time must be between 00:00 to 23:59")]
         public TimeSpan LessonStart { get; set; }
 
         [Required]
+         [JsonPropertyName("end_time")]
         [RegularExpression(@"((([0-1][0-9])|(2[0-3]))(:[0-5][0-9])(:[0-5][0-9])?)", 
             ErrorMessage = "Time must be between 00:00 to 23:59")]
         public TimeSpan LessonEnd { get; set; }
 
         [Required]
+        [JsonPropertyName("repeat_rate")]  
         public RepeatRate RepeatRate { get; set; }
 
+        [JsonPropertyName("day_number")] 
         [Range(1, 31)]   
         public uint? DayNumber { get; set; }
     }
