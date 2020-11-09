a8a49fdaa426b5748abb614f2eeb7782794b79e9
SerjKocherga <serj911kocherga@gmail.com>
2020-11-09 20:29:09 +0200

    code clean fix

--- CharlieBackend.Api/AddEasyNetQExtension.cs
+++ CharlieBackend.Api/AddEasyNetQExtension.cs
@@ -3,11 +3,6 @@ using EasyNetQ.AutoSubscribe;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
 
 namespace CharlieBackend.Api
 {
@@ -18,16 +13,5 @@ namespace CharlieBackend.Api
             var bus = RabbitHutch.CreateBus(rabbitmqConnectionString);
             service.AddSingleton<IBus>(bus);
         }
-
-        //public static void UseEasyNetQ(this IApplicationBuilder app)
-        //{
-        //    var bus = app.ApplicationServices.GetRequiredService<IBus>();
-        //    var autoSubscriber = new AutoSubscriber(bus, "productor")
-        //    {
-        //        AutoSubscriberMessageDispatcher = app.ApplicationServices.GetRequiredService<IAutoSubscriberMessageDispatcher>()
-        //    };
-        //    autoSubscriber.Subscribe(Assembly.GetExecutingAssembly());
-        //    autoSubscriber.SubscribeAsync(Assembly.GetExecutingAssembly());
-        //}
     }
 }
