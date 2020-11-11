e17dca7483eacda4dca0ed09a5fabd1cb613dcd1
pribytkova4769 <pribytkova4769@gmail.com>
2020-11-11 23:30:34 +0200

    Add StudentGroupFileModel

---
+++ CharlieBackend.Core/FileModels/StudentGroupFileModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CharlieBackend.Core.FileModels
+{
+    public class StudentGroupFileModel
+    {
+        public long? Id { get; set; }
+
+        public long CourseId { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime? StartDate { get; set; }
+
+        public DateTime? FinishDate { get; set; }
+    }
+}
