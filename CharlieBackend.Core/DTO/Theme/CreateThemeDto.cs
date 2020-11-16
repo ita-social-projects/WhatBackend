3104367c11164117ff1ec879921361ecd05716ac
Vladyslav Volkovskyi <volkovskiy4@gmail.com>
2020-11-16 14:18:45 +0200

    fixed theme dto

--- CharlieBackend.Core/DTO/Theme/CreateThemeDto.cs
+++ CharlieBackend.Core/DTO/Theme/CreateThemeDto.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel.DataAnnotations;
 
 namespace CharlieBackend.Core.DTO.Theme
 {
     public class CreateThemeDto
     {
+        [Required]
         public string Name { get; set; }
     }
 }
