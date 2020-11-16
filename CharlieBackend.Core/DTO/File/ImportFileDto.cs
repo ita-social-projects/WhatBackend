8ce047dec31a0f7f6b9db8c35ecab0deb4328919
pribytkova4769 <pribytkova4769@gmail.com>
2020-11-16 14:28:12 +0200

    fix annotations in File dto

--- CharlieBackend.Core/DTO/File/ImportFileDto.cs
+++ CharlieBackend.Core/DTO/File/ImportFileDto.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace CharlieBackend.Core.DTO.File
 {
     public class ImportFileDto
     {
+        [Required]
         public string url { get; set; }
     }
 }
