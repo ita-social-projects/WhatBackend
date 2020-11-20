dcb5cb8a772aaca207bb05910335730ea077be41
sober607 <55458570+sober607@users.noreply.github.com>
2020-11-20 14:54:06 +0200

    Add Swashbuckle, XML comments (#163)
    * Added Swashbuckle, XML comments to Secretaries controller, sample requests and responses
    
    * Refactoring
    
    * Small adjustments
    
    * Removed Library project

--- CharlieBackend.Api/Controllers/SecretariesController.cs
+++ CharlieBackend.Api/Controllers/SecretariesController.cs
@@ -4,11 +4,14 @@ using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using CharlieBackend.Core.DTO.Secretary;
 using Microsoft.AspNetCore.Authorization;
-using CharlieBackend.Core.Models.ResultModel;
+using Swashbuckle.AspNetCore.Annotations;
 using CharlieBackend.Business.Services.Interfaces;
 
 namespace CharlieBackend.Api.Controllers
 {
+    /// <summary>
+    /// Controller for operations related to secretary entity
+    /// </summary>
     [Route("api/secretaries")]
     [ApiController]
     public class SecretariesController : ControllerBase
@@ -17,13 +20,23 @@ namespace CharlieBackend.Api.Controllers
         private readonly ISecretaryService _secretaryService;
         private readonly IAccountService _accountService;
         #endregion
-
+        /// <summary>
+        /// SecretariesController constructor to inject related services
+        /// </summary>
         public SecretariesController(ISecretaryService secretaryService, IAccountService accountService)
         {
             _secretaryService = secretaryService;
             _accountService = accountService;
         }
 
+        /// <summary>
+        /// Gives secretary role to account and creates secretary entity
+        /// </summary>
+        /// <param name="accountId">Account id to approve</param>
+        /// <response code="200">Successful secretary entity create</response>
+        /// <response code="400">Account already assigned to secretary</response>
+        /// <response code="404">Acccount not found</response>
+        [SwaggerResponse(200, type: typeof(SecretaryDto))]
         [Authorize(Roles = "Admin")]
         [HttpPost("{accountId}")]
         public async Task<ActionResult> PostSecretary(long accountId)
@@ -33,7 +46,12 @@ namespace CharlieBackend.Api.Controllers
             return createdSecretaryDto.ToActionResult();
         }
 
+        /// <summary>
+        /// Gets all secretaries
+        /// </summary>
+        /// <response code="200">Returns list of all secretaries</response>
         [Authorize(Roles = "Admin, Secretary")]
+        [SwaggerResponse(200, type: typeof(List<SecretaryDto>))]
         [HttpGet]
         public async Task<ActionResult> GetAllSecretaries()
         {
@@ -42,15 +60,27 @@ namespace CharlieBackend.Api.Controllers
             return secretariesDtos.ToActionResult();
         }
 
+        /// <summary>
+        /// Updates exact secretary entity
+        /// </summary>
+        /// <response code="200">Returns updated data of secretary</response>
+        /// <response code="404">Secretary not found</response>
+        /// <response code="409">Email already taken</response>
+        [SwaggerResponse(200, type: typeof(UpdateSecretaryDto))]
         [Authorize(Roles = "Admin")]
         [HttpPut("{secretaryId}")]
-        public async Task<ActionResult> PutSecretary(long secretaryId, UpdateSecretaryDto secretaryDto)
+        public async Task<ActionResult> PutSecretary(long secretaryId, [FromBody]UpdateSecretaryDto secretaryDto)
         {
             var updatedSecretary = await _secretaryService.UpdateSecretaryAsync(secretaryId, secretaryDto);
 
             return updatedSecretary.ToActionResult();
         }
 
+        /// <summary>
+        /// Disable secretary entity
+        /// </summary>
+        /// <response code="200">Secretary successfully disabled</response>
+        /// <response code="404">Secretary not found</response>
         [Authorize(Roles = "Admin")]
         [HttpDelete("{secretaryId}")]
         public async Task<ActionResult> DisableSecretary(long secretaryId)
@@ -59,6 +89,5 @@ namespace CharlieBackend.Api.Controllers
 
             return isDisabled.ToActionResult();
         }
-
     }
 }
---
+++ CharlieBackend.Api/SwaggerExamples/SecretariesController/PostSecretaryRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Swashbuckle.AspNetCore.Filters;
+using CharlieBackend.Core.DTO.Secretary;
+
+namespace CharlieBackend.Api.SwaggerExamples.SecretariesController
+{
+    public class PostSecretaryRequest : IExamplesProvider<SecretaryDto>
+    {
+        public SecretaryDto GetExamples()
+        {
+            return new SecretaryDto()
+            {
+                Id = 145,
+                Email = "secretaryemail@example.com",
+                FirstName = "Isabella",
+                LastName = "Smith",
+            };
+        }
+    }
+}
