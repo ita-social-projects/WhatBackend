8f9ed858dfadc9aeb26b37524100eda4230fbc08
SerjKocherga <31800421+SerjKocherga@users.noreply.github.com>
2020-11-24 07:26:05 +0200

    Feature/add registration event (#175)
    * registration event impl, separate service for mail body
    
    * EventConsumer hanldes 2 types of events with auotosubscribe. Now while application is stopping, dispose makes unsubscribe for IBus.
    
    * some fixes for change request

--- WhatBackend.EmailRenderService/WhatBackend.EmailRenderService/Startup.cs
+++ WhatBackend.EmailRenderService/WhatBackend.EmailRenderService/Startup.cs
@@ -6,9 +6,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
+using WhatBackend.EmailRenderService.Services;
 using Microsoft.Extensions.DependencyInjection;
 using WhatBackend.EmailRenderService.IntegrationEvents;
+using WhatBackend.EmailRenderService.Services.Interfaces;
 using WhatBackend.EmailRenderService.IntegrationEvents.EventHandling;
+using CharlieBackend.Core.IntegrationEvents.Events;
+using System;
 
 namespace WhatBackend.EmailRenderService
 {
@@ -24,10 +28,13 @@ namespace WhatBackend.EmailRenderService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            
             services.AddCors();
+            services.AddSingleton<IMessageTemplateService, MessageTemplateService>();
             services.AddSingleton(RabbitHutch.CreateBus(Configuration.GetConnectionString("RabbitMQ")));
             services.AddSingleton<MessageDispatcher>();
-            services.AddScoped<AccountApprovedConsumer>();
+            services.AddScoped<EventConsumer>();
+
             services.AddSingleton(provider =>
             {
                 var subscriber = new AutoSubscriber(provider.GetRequiredService<IBus>(), "EmailRenderService")
@@ -43,9 +50,16 @@ namespace WhatBackend.EmailRenderService
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(
             IApplicationBuilder app,
-            IWebHostEnvironment env
+            IWebHostEnvironment env,
+            IHostApplicationLifetime applicationLifetime,
+            IBus bus
             )
         {
+            //When the application is stopping we need to make unsubscribe for IBus.
+            //Important! Even in debugg mode finish this application with Ctrl+C in a console to make sure
+            //what unsubscribe was done well.
+            applicationLifetime.ApplicationStopping.Register(OnShutdown, bus);
+
             //registering message consumers
             app.ApplicationServices.GetRequiredService<AutoSubscriber>().SubscribeAsync(new[] { Assembly.GetExecutingAssembly() });
 
@@ -65,6 +79,11 @@ namespace WhatBackend.EmailRenderService
             app.UseSerilogRequestLogging();
 
             app.UseRouting();
+        }
+
+        private void OnShutdown(object toDispose)
+        {
+            ((IDisposable)toDispose).Dispose();
         }
     }
 }
