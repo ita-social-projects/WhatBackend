67c3ea40b91f3057f9e3bc92fdf63254409f504e
SerjKocherga <serj911kocherga@gmail.com>
2020-12-04 19:59:05 +0200

    dto added

---
+++ CharlieBackend.Core/DTO/Account/ChangeCurrentPasswordDto.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CharlieBackend.Core.DTO.Account
+{
+    public class ChangeCurrentPasswordDto
+    {
+        [Required]
+        [EmailAddress]
+        [StringLength(50)]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        [DataType(DataType.Password)]
+        public string CurrentPassword { get; set; } 
+
+        [Required]
+        [StringLength(30, ErrorMessage = "Password should has at least 6 symbols.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+
+        [Required]
+        [StringLength(30, ErrorMessage = "Password should has at least 6 symbols.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Compare(nameof(NewPassword), ErrorMessage = "Passwords do not match.")]
+        public string ConfirmNewPassword { get; set; }
+    }
+}
