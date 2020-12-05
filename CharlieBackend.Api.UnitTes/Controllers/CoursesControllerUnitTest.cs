8849bda34dac47a99969f1780e47e817abdf4ccd
Hlib-mats <gleb.matckevich@gmail.com>
2020-12-05 20:11:15 +0200

    Update CoursesControllerUnitTest.cs

--- CharlieBackend.Api.UnitTes/Controllers/CoursesControllerUnitTest.cs
+++ CharlieBackend.Api.UnitTes/Controllers/CoursesControllerUnitTest.cs
@@ -11,37 +11,39 @@ using CharlieBackend.Core.Models.ResultModel;
 namespace CharlieBackend.Api.Controllers.Tests
 {
     public class CoursesControllerUnitTest
-	{
-		[Fact]
-		public async Task GetAllCoursesTestAsync()
-		{
-			//Arrange
+    {
 
-			var courceServiceMock = new Mock<ICourseService>();
-			courceServiceMock.Setup(repo => repo.GetAllCoursesAsync()).Returns(GetCourses());
-			CoursesController controller = new CoursesController(courceServiceMock.Object);
+        [Fact]
+        public async Task GetAllCoursesTestAsync()
+        {
+            //Arrange
 
-			//Act
+            var courceServiceMock = new Mock<ICourseService>();
+            courceServiceMock.Setup(repo => repo.GetAllCoursesAsync()).Returns(GetCourses());
+            CoursesController controller = new CoursesController(courceServiceMock.Object);
 
-			var GetResult = controller.GetAllCourses();
-			var objectResult = GetResult.Result.Result as ObjectResult;
-			var toCompare = objectResult.Value as List<CourseDto>;
-			var actualResult = await GetCourses();
+            //Act
 
-			//Assert
+            var GetResult = controller.GetAllCourses();
+            var objectResult = GetResult.Result.Result as ObjectResult;
+            var toCompare = objectResult.Value as List<CourseDto>;
+            var actualResult = await GetCourses();
 
-			Assert.Equal(toCompare.Count, actualResult.Data.Count);
-		}
+            //Assert
 
-		public async Task<Result<IList<CourseDto>>> GetCourses()
-		{
-			List<CourseDto> coursesL = new List<CourseDto>
-			{
-				new CourseDto { Id = 12, Name = "Charli" },
-				new CourseDto { Id = 13, Name = "Alfa" },
-				new CourseDto { Id = 14, Name = "Omega" }
-			};
-			return Result < IList < CourseDto >>.GetSuccess(coursesL);
-		}	
-	}
+            Assert.Equal(toCompare.Count, actualResult.Data.Count);
+        }
+
+        public async Task<Result<IList<CourseDto>>> GetCourses()
+        {
+            List<CourseDto> coursesL = new List<CourseDto>
+            {
+                new CourseDto { Id = 12, Name = "Charli" },
+                new CourseDto { Id = 13, Name = "Alfa" },
+                new CourseDto { Id = 14, Name = "Omega" }
+            };
+            return Result<IList<CourseDto>>.GetSuccess(coursesL);
+        }
+
+    }
 }
