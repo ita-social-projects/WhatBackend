bfe241aa0000b48216a5bb85df5e34544ac1970d
Vladyslav Volkovskyi <volkovskiy4@gmail.com>
2020-12-07 00:11:55 +0200

    add new dto

---
+++ CharlieBackend.Core/DTO/Mentor/MentorStudyGroupsDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CharlieBackend.Core.DTO.Mentor
+{
+    class MentorStudyGroupsDto
+    {
+        public long Id { get; set; }
+
+        [StringLength(100)]
+        public string Name { get; set; }
+    }
+}
