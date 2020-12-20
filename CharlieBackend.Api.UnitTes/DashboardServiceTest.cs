0b1787460ccf34f97b0ae41abb0f8a259434c4ac
sober607 <55458570+sober607@users.noreply.github.com>
2020-12-20 18:51:32 +0200

    Partial test

---
+++ CharlieBackend.Api.UnitTes/DashboardServiceTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using CharlieBackend.Business.Services.Interfaces;
+using CharlieBackend.Core.DTO.Dashboard;
+using CharlieBackend.Data.Repositories.Impl.Interfaces;
+using Moq;
+using Xunit;
+
+namespace CharlieBackend.Api.UnitTest
+{
+    public class DashboardServiceTest : TestBase
+    {
+        private readonly Mock<IDashboardService> _dashboardServiceMock;
+
+        [Fact]
+        public async Task GetStudentsClassbook()
+        {
+            //Arrange
+            var studentclassbookRequestWithData = new StudentsRequestDto<ClassbookResultType>()
+            {
+                CourseId = 5,
+                StartDate = new DateTime(2010, 01, 01),
+                FinishDate = new DateTime(2021, 01, 01),
+                IncludeAnalytics = new ClassbookResultType[]
+                {
+                    ClassbookResultType.StudentPresence,
+                    ClassbookResultType.StudentMarks
+                }
+            };
+
+            var studentclassbookWrongRequest = new StudentsRequestDto<ClassbookResultType>()
+            {
+                StartDate = new DateTime(2010, 01, 01),
+                FinishDate = new DateTime(2021, 01, 01),
+            };
+
+            var dashboardRepositoryMock = new Mock<IDashboardRepository>();
+
+            dashboardRepositoryMock.Setup(x => x.GetGroupsIdsByCourseIdAsync(
+                studentclassbookRequestWithData.CourseId.Value,
+                studentclassbookRequestWithData.StartDate,
+                studentclassbookRequestWithData.FinishDate)).ReturnsAsync(new List<long>() { 1, 5 });
+
+            dashboardRepositoryMock.Setup(x => x.GetStudentsIdsByGroupIdsAsync())
+
+            //Act
+        }
+
+        protected override Mock<IUnitOfWork> GetUnitOfWorkMock()
+        {
+            var mock = new Mock<IUnitOfWork>();
+            return mock;
+        }
+    }
+}
