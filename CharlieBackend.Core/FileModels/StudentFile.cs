9be8fdc6a63a5446f6b07632a5bfa3a1144a0a50
Hlib-mats <gleb.matckevich@gmail.com>
2020-12-22 15:01:51 +0200

    Added someValidation in Files

--- CharlieBackend.Core/FileModels/StudentFile.cs
+++ CharlieBackend.Core/FileModels/StudentFile.cs
@@ -1,12 +1,20 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations;
+
 namespace CharlieBackend.Core.FileModels
 {
     public class StudentFile
     {
+        [Required]
+        [EmailAddress]
+        [StringLength(50)]
         public string Email { get; set; }
 
+        [Required]
+        [StringLength(30)]
         public string FirstName { get; set; }
 
+        [Required]
+        [StringLength(30)]
         public string LastName { get; set; }
     }
 }
