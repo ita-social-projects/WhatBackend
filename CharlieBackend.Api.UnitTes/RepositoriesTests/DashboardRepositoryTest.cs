1e90cc256e2c02ff92f68f892d1ad06fd2f49bba
Hlib-mats <gleb.matckevich@gmail.com>
2020-12-30 17:43:23 +0200

    first Succes Test(Get groups Id)

--- CharlieBackend.Api.UnitTes/RepositoriesTests/DashboardRepositoryTest.cs
+++ CharlieBackend.Api.UnitTes/RepositoriesTests/DashboardRepositoryTest.cs
@@ -8,19 +8,21 @@ using System;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using MockQueryable.Moq;
 
 namespace CharlieBackend.Api.UnitTest.RepositoriesTests
 {
     public class DashboardRepositoryTest : TestBase
     {
+        //короче тут мок констекста
+        // ему на вход идет опшионс
         private readonly Mock<ApplicationContext> _applicationContextMock;
-        private readonly Mock<DbSet<StudentGroup>> _dbSetStudGroupMock;
 
         public DashboardRepositoryTest()
         {
             var optionsBuilder = new DbContextOptionsBuilder<ApplicationContext>();
             _applicationContextMock = new Mock<ApplicationContext>(optionsBuilder.Options);
-            _dbSetStudGroupMock = new Mock<DbSet<StudentGroup>>();
         }
 
         [Fact]
@@ -28,97 +30,104 @@ namespace CharlieBackend.Api.UnitTest.RepositoriesTests
         {
             // Arrange
 
-            var inRangeStdentGroupFirst = new StudentGroup()
+            var groups = new List<StudentGroup> 
             {
-                CourseId = 1,
-                StartDate = new DateTime(2000, 10, 1),
-                FinishDate = new DateTime(2001, 1, 20),
-                Id = 1,
-                StudentsOfStudentGroups = new List<StudentOfStudentGroup>()
-                {
-                    new StudentOfStudentGroup()
+
+                 new StudentGroup()
+                 {
+                    CourseId = 1,
+                     StartDate = new DateTime(2000, 10, 1),
+                     FinishDate = new DateTime(2001, 1, 20),
+                     Id = 1,
+                     StudentsOfStudentGroups = new List<StudentOfStudentGroup>()
+                     {
+                        new StudentOfStudentGroup()
+                        {
+                            StudentId = 10,
+                        },
+                        new StudentOfStudentGroup()
+                        {
+                            StudentId = 11,
+                        },
+                        new StudentOfStudentGroup()
+                        {
+                            StudentId = 12,
+                        },
+                        new StudentOfStudentGroup()
+                        {
+                            StudentId = 13,
+                        }
+                     }
+                 },
+
+                 new StudentGroup()
+                 {
+                    CourseId = 1,
+                    StartDate = new DateTime(2000, 11, 4),
+                    FinishDate = new DateTime(2001, 1, 10),
+                    Id = 4,
+                    StudentsOfStudentGroups = new List<StudentOfStudentGroup>()
                     {
-                        StudentId = 10,
-                    },
-                    new StudentOfStudentGroup()
-                    {
-                        StudentId = 11,
-                    },
-                    new StudentOfStudentGroup()
-                    {
-                        StudentId = 12,
-                    },
-                    new StudentOfStudentGroup()
-                    {
-                        StudentId = 13,
+                        new StudentOfStudentGroup()
+                        {
+                            StudentId = 4,
+                        },
+                        new StudentOfStudentGroup()
+                        {
+                            StudentId = 5,
+                        },
+                        new StudentOfStudentGroup()
+                        {
+                            StudentId = 6,
+                        },
+                        new StudentOfStudentGroup()
+                        {
+                            StudentId = 20,
+                        }
                     }
-                }
+                 },
+
+                 new StudentGroup()
+                 {
+                    CourseId = 1,
+                    StartDate = new DateTime(2003, 1, 1),
+                    FinishDate = new DateTime(2006, 10, 2),
+                    Id = 5,
+                    StudentsOfStudentGroups = new List<StudentOfStudentGroup>()
+                    {
+                        new StudentOfStudentGroup()
+                        {
+                            StudentId = 10,
+                        },
+                        new StudentOfStudentGroup()
+                        {
+                            StudentId = 11,
+                        },
+                        new StudentOfStudentGroup()
+                        {
+                            StudentId = 12,
+                        },
+                        new StudentOfStudentGroup()
+                        {
+                            StudentId = 13,
+                        }
+                    }
+                 }
             };
 
-            var inRangeStdentGroupSecond = new StudentGroup()
-            {
-                CourseId = 1,
-                StartDate = new DateTime(2000, 11, 4),
-                FinishDate = new DateTime(2001, 1, 10),
-                Id = 4,
-                StudentsOfStudentGroups = new List<StudentOfStudentGroup>()
-                {
-                    new StudentOfStudentGroup()
-                    {
-                        StudentId = 4,
-                    },
-                    new StudentOfStudentGroup()
-                    {
-                        StudentId = 5,
-                    },
-                    new StudentOfStudentGroup()
-                    {
-                        StudentId = 6,
-                    },
-                    new StudentOfStudentGroup()
-                    {
-                        StudentId = 20,
-                    }
-                }
-            };
+            var mock = groups.AsQueryable().BuildMockDbSet();
+            
+            _applicationContextMock.Setup(x => x.StudentGroups).Returns(mock.Object);
 
-            var outOfRangeStdentGroup = new StudentGroup()
-            {
-                CourseId = 1,
-                StartDate = new DateTime(2003, 1, 1),
-                FinishDate = new DateTime(2006, 10, 2),
-                Id = 1,
-                StudentsOfStudentGroups = new List<StudentOfStudentGroup>()
-                {
-                    new StudentOfStudentGroup()
-                    {
-                        StudentId = 10,
-                    },
-                    new StudentOfStudentGroup()
-                    {
-                        StudentId = 11,
-                    },
-                    new StudentOfStudentGroup()
-                    {
-                        StudentId = 12,
-                    },
-                    new StudentOfStudentGroup()
-                    {
-                        StudentId = 13,
-                    }
-                }
-            };
+            var dashboardRepositort = new DashboardRepository(_applicationContextMock.Object);
 
-            //_applicationContextMock.Setup(x => x.);
-            _applicationContextMock.Setup(x => x.StudentGroups.AddRange(inRangeStdentGroupFirst, inRangeStdentGroupSecond, outOfRangeStdentGroup));
-
-            DashboardRepository dashboardRepository = new DashboardRepository(_applicationContextMock.Object);
             // Act
 
-            var result = await dashboardRepository.GetGroupsIdsByCourseIdAsync(1, new DateTime(2000, 9, 1), new DateTime(2001, 12, 12));
+            var result = await dashboardRepositort.GetGroupsIdsByCourseIdAsync(1, new DateTime(2000, 1, 1), new DateTime(2002, 1, 1));
 
             // Assert
-            Assert.Equal(new List<long> { 1, 2 }, result);
+            Assert.NotNull(result);
+            Assert.Equal(new List<long> { 1, 4 }, result);
         }
 
         protected override Mock<IUnitOfWork> GetUnitOfWorkMock()
