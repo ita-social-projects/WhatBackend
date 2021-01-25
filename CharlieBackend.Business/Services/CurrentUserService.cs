7bb2d2f8ce9379774a9910226365c1a1f1c564f1
gingerbread-monster <staslazlegolas@gmail.com>
2021-01-26 00:42:11 +0200

    add draft implementation of CurrentUserService #330

---
+++ CharlieBackend.Business/Services/CurrentUserService.cs
@@ -0,0 +1,86 @@
+﻿using CharlieBackend.Business.Services.Interfaces;
+using CharlieBackend.Core.Entities;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Linq;
+using System.Security.Claims;
+
+namespace CharlieBackend.Business.Services
+{
+    /// <summary>
+    /// Class that provides access to properties of the current user.
+    /// </summary>
+    public class CurrentUserService : ICurrentUserService
+    {
+        readonly IHttpContextAccessor _httpContextAccessor;
+
+        public CurrentUserService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public long AccountId
+        {
+            get
+            {
+                string accountIdString = _httpContextAccessor
+                    .HttpContext
+                    .User
+                    ?.Claims
+                    ?.SingleOrDefault(claim => claim.Type == "AccountId")
+                    ?.Value;
+
+                long accountId;
+
+                if (!long.TryParse(accountIdString, out accountId))
+                {
+                    throw new UnauthorizedAccessException("Not authorized!");
+                }
+
+                return accountId;
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                string email = _httpContextAccessor
+                    .HttpContext
+                    .User
+                    ?.Claims
+                    ?.SingleOrDefault(claim => claim.Type == ClaimTypes.Email)
+                    ?.Value;
+
+                if (email is null)
+                {
+                    throw new UnauthorizedAccessException("Not authorized!");
+                }
+
+                return email;
+            }
+        }
+
+        public UserRole Role
+        {
+            get
+            {
+                string roleString = _httpContextAccessor
+                    .HttpContext
+                    .User
+                    ?.Claims
+                    ?.SingleOrDefault(claim => claim.Type == ClaimsIdentity.DefaultRoleClaimType)
+                    ?.Value;
+
+                UserRole role;
+
+                if (!Enum.TryParse<UserRole>(roleString, out role))
+                {
+                    throw new UnauthorizedAccessException("Not authorized!");
+                }
+
+                return role;
+            }
+        }
+    }
+}
---
+++ CharlieBackend.Business/Services/Interfaces/ICurrentUserService.cs
@@ -0,0 +1,25 @@
+﻿using CharlieBackend.Core.Entities;
+
+namespace CharlieBackend.Business.Services.Interfaces
+{
+    /// <summary>
+    /// Interface that describes accessible properties of the current user.
+    /// </summary>
+    public interface ICurrentUserService
+    {
+        /// <summary>
+        /// Property that reflects the account Id of the current user.
+        /// </summary>
+        public long AccountId { get; }
+
+        /// <summary>
+        /// Property that reflects the Email of the current user.
+        /// </summary>
+        public string Email { get; }
+
+        /// <summary>
+        /// Property that reflects the role of the current user.
+        /// </summary>
+        public UserRole Role { get; }
+    }
+}
