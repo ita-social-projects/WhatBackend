ec06646f2d807e044968f9a66b66dee9bc86f706
CptHouston <penguin.ua@gmail.com>
2021-01-27 11:16:08 +0200

    -- updated parser to increase readability

--- CharlieBackend.Business/Services/ScheduleServiceFolder/EventOccuranceStorageParser.cs
+++ CharlieBackend.Business/Services/ScheduleServiceFolder/EventOccuranceStorageParser.cs
@@ -20,42 +20,99 @@ namespace CharlieBackend.Business.Services
 
         internal static long GetPatternStorageValue(PatternForCreateScheduleDTO source)
         {
-            long result = 0;
-            long index = 1;
+            (long result, long position) data = (0, 1);
+
+            return data.AddWeekDays(source)
+                .AddMonthIndex(source)
+                .AddDates(source)
+                .AddInterval(source);
+        }
+
+        private static (long result, long position) AddWeekDays(this (long result, long index) data, PatternForCreateScheduleDTO source)
+        {
+            long defaultValue = data.index;
 
             if (source.DaysOfWeek != null)
-                foreach (DayOfWeek item in source.DaysOfWeek)
             {
-                index <<= (int)item;
-                result |= index;
-                index = 1;
+                foreach (DayOfWeek item in source.DaysOfWeek)
+                {
+                    data.index <<= (int)item;
+                    data.result |= data.index;
+                    data.index = defaultValue;
+                }
             }
 
-            if (source.Index.HasValue)
-                result |= index << (_daysInWeek + (int)source.Index);
+            data.index <<= _daysInWeek;
 
-            index <<= _daysInWeek + _possibleMonthIndexOptionsNumber;
+            return data;
+        }
+
+        private static (long result, long index) AddMonthIndex(this (long result, long index) data, PatternForCreateScheduleDTO source)
+        {
+            if (source.Index.HasValue)
+            {
+                data.result |= data.index << (int)source.Index;
+            }
+
+            data.index <<= _possibleMonthIndexOptionsNumber;
+
+            return data;
+        }
+
+        private static (long result, long index) AddDates(this (long result, long index) data, PatternForCreateScheduleDTO source)
+        {
+            long defaultValue = data.index;
 
             if (source.Dates != null)
-                foreach (int item in source.Dates)
             {
-                index <<= item;
-                result |= index;
-                index = _daysInWeek + _possibleMonthIndexOptionsNumber;
+                foreach (int item in source.Dates)
+                {
+                    data.index <<= item;
+                    data.result |= data.index;
+                    data.index = defaultValue;
+                }
             }
 
-            index = 1;
-            result |= index << (_daysInWeek + _possibleMonthIndexOptionsNumber + _maxDaysInMonth + source.Interval);
+            data.index <<= _maxDaysInMonth;
 
-            return result;
+            return data;
+        }
+
+        private static long AddInterval(this (long result, long index) data, PatternForCreateScheduleDTO source)
+        {
+            ///Should be used last to set up final length
+            data.result |= data.index << source.Interval;
+
+            return data.result;
         }
 
         internal static PatternForCreateScheduleDTO GetFullDataFromStorage(long source)
         {
-            string check = Convert.ToString(source, 2);
+            string stringRepresentationOfStorage = Convert.ToString(source, 2);
 
-            string daysString = check.Substring(check.Length - _daysInWeek);
+            string daysString = stringRepresentationOfStorage
+                .Substring(stringRepresentationOfStorage.Length - _daysInWeek);
 
+            string indexString = stringRepresentationOfStorage
+                .Substring(stringRepresentationOfStorage.Length - _daysInWeek - _possibleMonthIndexOptionsNumber, _possibleMonthIndexOptionsNumber);
+
+            string datesString = stringRepresentationOfStorage
+                .Substring(stringRepresentationOfStorage.Length - _daysInWeek - _possibleMonthIndexOptionsNumber - _maxDaysInMonth, _maxDaysInMonth);
+
+            string intervalString = stringRepresentationOfStorage
+                .Substring(0, stringRepresentationOfStorage.Length - _daysInWeek - _possibleMonthIndexOptionsNumber - _maxDaysInMonth);
+
+            return new PatternForCreateScheduleDTO
+            {
+                Interval = GetInterval(intervalString),
+                Index = GetMonthIndex(indexString),
+                Dates = GetDatesList(datesString),
+                DaysOfWeek = GetDaysList(daysString)
+            };
+        }
+
+        private static IList<DayOfWeek> GetDaysList(string daysString)
+        {
             IList<DayOfWeek> daysCollection = new List<DayOfWeek>();
 
             for (int i = daysString.Length - 1; i >= 0; i--)
@@ -66,13 +123,16 @@ namespace CharlieBackend.Business.Services
                 }
             }
 
-            string indexString = check.Substring(check.Length - _daysInWeek - _possibleMonthIndexOptionsNumber, _possibleMonthIndexOptionsNumber);
+            return daysCollection;
+        }
 
-            MonthIndex index = (MonthIndex)(indexString.Length - indexString.LastIndexOf('1') - 1);
+        private static MonthIndex GetMonthIndex(string indexString)
+        {
+            return (MonthIndex)(indexString.Length - indexString.LastIndexOf('1') - 1);
+        }
 
-            string datesString = check.Substring(check.Length - _daysInWeek - _possibleMonthIndexOptionsNumber - _maxDaysInMonth, 
-                _maxDaysInMonth);
-
+        private static IList<int> GetDatesList(string datesString)
+        {
             IList<int> datesCollection = new List<int>();
 
             for (int i = datesString.Length - 1; i >= 0; i--)
@@ -83,17 +143,12 @@ namespace CharlieBackend.Business.Services
                 }
             }
 
-            string intervalString = check.Substring(0, check.Length - _daysInWeek - _possibleMonthIndexOptionsNumber - _maxDaysInMonth);
+            return datesCollection;
+        }
 
-            int interval = intervalString.Length - intervalString.LastIndexOf('1') - 1;
-
-            return new PatternForCreateScheduleDTO
-            {
-                Interval = interval,
-                Index = index,
-                Dates = datesCollection,
-                DaysOfWeek = daysCollection
-            };
+        private static int GetInterval(string intervalString)
+        {
+            return intervalString.Length - intervalString.LastIndexOf('1') - 1;
         }
     }
 }
