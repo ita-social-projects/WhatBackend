131a7b292259fa8428f5b645e45c0ac5ce5f7f92
QuestroL <49345889+QuestroL@users.noreply.github.com>
2021-02-09 19:27:16 +0200

    fix db script

--- scripts/generate_database.sql
+++ scripts/generate_database.sql
@@ -120,18 +120,17 @@ CREATE TABLE `event_occurence` (
 DROP TABLE IF EXISTS `homework`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `homework` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `due_date` datetime DEFAULT NULL,
-  `task_text` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
-  `student_group_id` bigint NOT NULL,
-  `mentor_id` bigint NOT NULL,
+CREATE TABLE IF NOT EXISTS `soft`.`homework` (
+  `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+  `due_date` DATETIME NULL DEFAULT NULL,
+  `task_text` TEXT CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_0900_ai_ci' NULL DEFAULT NULL,
+  `lesson_id` BIGINT(20) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `FK_student_group_of_homework` (`student_group_id`),
-  KEY `FK_mentor_of_homework` (`mentor_id`),
-  CONSTRAINT `FK_mentor_of_homework` FOREIGN KEY (`mentor_id`) REFERENCES `mentor` (`id`),
-  CONSTRAINT `FK_student_group_of_homework` FOREIGN KEY (`student_group_id`) REFERENCES `student_group` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  INDEX `FK_lesson_of_homework` (`lesson_id` ASC),
+  CONSTRAINT `FK_lesson_of_homework`
+    FOREIGN KEY (`lesson_id`)
+    REFERENCES `soft`.`lesson` (`id`))
+ ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -251,12 +250,6 @@ DROP TABLE IF EXISTS `secretary`;
 CREATE TABLE `secretary` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `account_id` bigint NOT NULL,
-----------------------
--- Table `soft`.`student`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `soft`.`student` (
-  `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
-  `account_id` BIGINT(20) NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `FK_account_of_secretary` (`account_id`),
   CONSTRAINT `FK_account_of_secretary` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`)
@@ -348,18 +341,12 @@ CREATE TABLE `visit` (
   `presence` tinyint(1) NOT NULL COMMENT 'presence default value has been set',
   `comment` varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   PRIMARY KEY (`id`),
-  INDEX `FK_homework_id` (`homework_id` ASC),
-  INDEX `FK_attachment_id` (`attachment_id` ASC),
-  CONSTRAINT `FK_homework_of_attachment`
-    FOREIGN KEY (`homework_id`)
-    REFERENCES `soft`.`homework` (`id`),
-  CONSTRAINT `FK_attachment_of_homework`
-    FOREIGN KEY (`attachment_id`)
-    REFERENCES `soft`.`attachment` (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
+  KEY `FK_lesson_of_visit` (`lesson_id`),
+  KEY `FK_student_of_visit_idx` (`student_id`),
+  CONSTRAINT `FK_lesson_of_visit` FOREIGN KEY (`lesson_id`) REFERENCES `lesson` (`id`),
+  CONSTRAINT `FK_student_of_visit` FOREIGN KEY (`student_id`) REFERENCES `student` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=400 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 -- -----------------------------------------------------
 -- Table `soft`.`homework_from_student`
@@ -377,11 +364,10 @@ CREATE TABLE IF NOT EXISTS `soft`.`homework_from_student` (
   CONSTRAINT `FK_homework_of_student`
     FOREIGN KEY (`student_id`)
     REFERENCES `soft`.`student` (`id`))
-    
+
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
-
 -- -----------------------------------------------------
 -- Table `soft`.`attachment_of_homework`
 -- -----------------------------------------------------
@@ -403,53 +389,20 @@ DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
 --
--- Table structure for table `event_occurence`
---
-DROP TABLE IF EXISTS `event_occurence`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `event_occurence` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `student_group_id` bigint NOT NULL,
-  `event_start` datetime NOT NULL,
-  `event_finish` datetime NOT NULL,
-  `pattern` int NOT NULL,
-  `storage` bigint unsigned NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK_student_group_of_schedule` (`student_group_id`),
-  CONSTRAINT `FK_student_group_of_schedule` FOREIGN KEY (`student_group_id`) REFERENCES `student_group` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `scheduled_event`
+-- Dumping events for database 'soft'
 --
 
-DROP TABLE IF EXISTS `scheduled_event`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `scheduled_event` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `event_occurence_id` bigint NOT NULL,
-  `student_group_id` bigint DEFAULT NULL,
-  `theme_id` bigint DEFAULT NULL,
-  `mentor_id` bigint DEFAULT NULL,
-  `lesson_id` bigint DEFAULT NULL,
-  `event_start` datetime NOT NULL,
-  `event_finish` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_scheduled_events_event_occurence1_idx` (`event_occurence_id`),
-  KEY `fk_scheduled_events_student_group1_idx` (`student_group_id`),
-  KEY `fk_scheduled_events_theme1_idx` (`theme_id`),
-  KEY `fk_scheduled_events_mentor1_idx` (`mentor_id`),
-  KEY `fk_scheduled_events_lesson1_idx` (`lesson_id`),
-  CONSTRAINT `fk_scheduled_events_event_occurence1` FOREIGN KEY (`event_occurence_id`) REFERENCES `event_occurence` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `fk_scheduled_events_lesson1` FOREIGN KEY (`lesson_id`) REFERENCES `lesson` (`id`),
-  CONSTRAINT `fk_scheduled_events_mentor1` FOREIGN KEY (`mentor_id`) REFERENCES `mentor` (`id`),
-  CONSTRAINT `fk_scheduled_events_student_group1` FOREIGN KEY (`student_group_id`) REFERENCES `student_group` (`id`),
-  CONSTRAINT `fk_scheduled_events_theme1` FOREIGN KEY (`theme_id`) REFERENCES `theme` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+--
+-- Dumping routines for database 'soft'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2021-01-26 14:27:34
