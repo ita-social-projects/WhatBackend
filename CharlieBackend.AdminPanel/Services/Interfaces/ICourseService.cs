9260c5cf890e8944603dadcaf89a5b759a1a7fa4
Андрій Миронов <dendyandy10@gmail.com>
2021-02-14 15:59:09 +0200

    Course Service

--- CharlieBackend.AdminPanel/Services/Interfaces/ICourseService.cs
+++ CharlieBackend.AdminPanel/Services/Interfaces/ICourseService.cs
@@ -1,13 +1,16 @@
 ﻿using CharlieBackend.AdminPanel.Models.Course;
-using System;
+using CharlieBackend.Core.DTO.Course;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace CharlieBackend.AdminPanel.Services.Interfaces
 {
     public interface ICourseService
     {
-        public Task<IList<CourseViewModel>> GetAllCoursesAsync();
+        Task<IList<CourseViewModel>> GetAllCoursesAsync();
+
+        Task<CourseDto> DisableCourseAsync(long id);
+
+        Task UpdateCourse(long id, UpdateCourseDto UpdateDto);
     }
 }
