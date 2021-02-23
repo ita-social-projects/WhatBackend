a48eed7492bd6dca6cd590770a0fa441771304fc
brainbeast <62809004+andy-dendy-repo@users.noreply.github.com>
2021-02-23 21:44:33 +0200

    Update AzureStorageBlobAccount.cs

--- CharlieBackend.Core/AzureStorageBlobAccount.cs
+++ CharlieBackend.Core/AzureStorageBlobAccount.cs
@@ -4,24 +4,9 @@
     {
         public readonly string ConnectionString;
 
-        public readonly string AccountName;
-
-        public readonly string AccountKey;
-
-        public readonly string EndpointSuffix;
-
-        public readonly string DefaultEndpointsProtocol;
-
         public AzureStorageBlobAccount(string connectionString)
         {
             ConnectionString = connectionString;
-        }
-
-        private void SetBlobAccountFields()
-        {
-            string[] dividedString = ConnectionString.Split(";");
-
-
         }
     }
 }
