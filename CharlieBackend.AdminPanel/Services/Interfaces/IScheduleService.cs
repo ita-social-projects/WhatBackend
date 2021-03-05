aa470f74bab31320fae8e46c6f9ccc5496cb8709
Андрій Миронов <dendyandy10@gmail.com>
2021-03-05 16:51:12 +0200

    IScheduleService is added to AdminPanel

---
+++ CharlieBackend.AdminPanel/Services/Interfaces/IScheduleService.cs
@@ -0,0 +1,13 @@
+﻿using CharlieBackend.Core.DTO.Schedule;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CharlieBackend.AdminPanel.Services.Interfaces
+{
+    public interface IScheduleService
+    {
+        Task<IList<ScheduledEventDTO>> GetEventsFiltered(ScheduledEventFilterRequestDTO request);
+    }
+}
