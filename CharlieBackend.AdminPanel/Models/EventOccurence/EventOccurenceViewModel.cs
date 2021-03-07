75aaf93edd33d6e625f71ee47fda8c906f5d60be
Андрій Миронов <dendyandy10@gmail.com>
2021-03-07 18:07:26 +0200

    EventOccurenceViewModel added

--- CharlieBackend.AdminPanel/Models/EventOccurence/EventOccurenceViewModel.cs
+++ CharlieBackend.AdminPanel/Models/EventOccurence/EventOccurenceViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CharlieBackend.Core.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,14 @@ namespace CharlieBackend.AdminPanel.Models.EventOccurence
 {
     public class EventOccurenceViewModel
     {
+        public long Id { get; set; }
+
+        public DateTime EventStart { get; set; }
+
+        public DateTime EventFinish { get; set; }
+
+        public PatternType Pattern { get; set; }
+
+        public long Storage { get; set; }
     }
 }
