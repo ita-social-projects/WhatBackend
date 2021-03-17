a464d5562ac8934f2672925f553ab20e2a315825
gingerbread-monster <staslazlegolas@gmail.com>
2021-03-17 18:47:15 +0200

    add warning 1591 to 'no warning list' #372

--- CharlieBackend.Api/Middlewares/IsAccountActiveMiddleware.cs
+++ CharlieBackend.Api/Middlewares/IsAccountActiveMiddleware.cs
@@ -1,11 +1,6 @@
-using CharlieBackend.Business.Services.Interfaces;
-using CharlieBackend.Core.Entities;
-using CharlieBackend.Api.Extensions;
-using System.IdentityModel.Tokens.Jwt;
+﻿using CharlieBackend.Business.Services.Interfaces;
 using Microsoft.AspNetCore.Http;
-using System.Security.Claims;
 using System.Threading.Tasks;
-using System.Linq;
 
 namespace CharlieBackend.Api.Middlewares
 {
@@ -14,22 +9,17 @@ namespace CharlieBackend.Api.Middlewares
     /// </summary>
     public class IsAccountActiveMiddleware
     {
-        #region
         private readonly RequestDelegate _next;
-        #endregion
 
-        /// <summary>
-        /// 
-        /// </summary>
         public IsAccountActiveMiddleware(RequestDelegate next)
         {
             this._next = next;
         }
 
-        /// <summary>
-        /// 
-        /// </summary>
-        public async Task InvokeAsync(HttpContext context, IAccountService accountService, ICurrentUserService currentUserService)
+        public async Task InvokeAsync(
+            HttpContext context, 
+            IAccountService accountService, 
+            ICurrentUserService currentUserService)
         {
             if (context.Request.Path.Value.Contains("accounts")
                 || context.Request.Path.Value.Contains("swagger"))
@@ -41,15 +31,15 @@ namespace CharlieBackend.Api.Middlewares
                 var currentEmail = currentUserService.Email;
 
                 var isActive = await accountService.IsAccountActiveAsync(currentEmail);
-                
+                    
                 if ((bool)!isActive)
                 {
                      context.Response.StatusCode = StatusCodes.Status403Forbidden;
 
                      await context.Response.WriteAsync("Account is deactivated");
-                }    
-
-                    await _next.Invoke(context);
+                }
+                
+                await _next.Invoke(context);
             }
         }
     }
