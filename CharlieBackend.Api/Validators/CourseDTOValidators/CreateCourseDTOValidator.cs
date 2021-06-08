26ea3851745dbf14c14a1f39b104c9017dcfcc2b
Dmitry Krukovsky <megabelka1@gmail.com>
2021-06-08 14:23:14 +0300

    CourseDtoValidators

---
+++ CharlieBackend.Api/Validators/CourseDTOValidators/CreateCourseDTOValidator.cs
@@ -0,0 +1,16 @@
+﻿using CharlieBackend.Business.Helpers;
+using CharlieBackend.Core.DTO.Course;
+using FluentValidation;
+
+namespace CharlieBackend.Api.Validators.CourseDTOValidators
+{
+    public class CreateCourseDTOValidator : AbstractValidator<CreateCourseDto>
+    {
+        public CreateCourseDTOValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .MaximumLength(ValidationConstants.MaxLengthName);
+        }
+    }
+}
