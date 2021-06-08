e9816f74b705b71c5005cc39455797fa23ae290a
Dmitry Krukovsky <megabelka1@gmail.com>
2021-06-08 16:01:42 +0300

    EmailData DTO Validator

---
+++ CharlieBackend.Api/Validators/EmailDataDTOValidator/EmailDataDtoValidator.cs
@@ -0,0 +1,20 @@
+﻿using CharlieBackend.Business.Helpers;
+using CharlieBackend.Core.DTO.Course;
+using CharlieBackend.Core.DTO.EmailData;
+using FluentValidation;
+
+namespace CharlieBackend.Api.Validators.EmailDataDTOValidator
+{
+    public class EmailDataDtoValidator : AbstractValidator<EmailData>
+    {
+        public EmailDataDtoValidator()
+        {
+            RuleFor(x => x.RecipientMail)
+                .NotEmpty()
+                .EmailAddress()
+                .MaximumLength(ValidationConstants.MaxLengthEmail);
+            RuleFor(x => x.EmailBody)
+                .NotEmpty();
+        }
+    }
+}
