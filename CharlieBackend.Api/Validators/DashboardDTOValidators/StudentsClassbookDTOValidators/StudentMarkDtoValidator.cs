076b2235f11649684d0122db1e71dc29161f7890
Dmitry Krukovsky <megabelka1@gmail.com>
2021-06-08 15:43:55 +0300

    DashboardDtoValidators

---
+++ CharlieBackend.Api/Validators/DashboardDTOValidators/StudentsClassbookDTOValidators/StudentMarkDtoValidator.cs
@@ -0,0 +1,40 @@
+﻿using CharlieBackend.Business.Helpers;
+using CharlieBackend.Core.DTO.Dashboard;
+using FluentValidation;
+using System;
+
+namespace CharlieBackend.Api.Validators.DashboardDTOValidators.StudentsClassbookDTOValidators
+{
+    public class StudentMarkDtoValidator : AbstractValidator<StudentMarkDto>
+    {
+        public StudentMarkDtoValidator()
+        {
+            RuleFor(x => x.CourseId)
+               .NotNull()
+               .GreaterThan(0);
+            RuleFor(x => x.StudentGroupId)
+                .NotNull()
+                .GreaterThan(0);
+            RuleFor(x => x.StudentId)
+              .NotNull()
+              .GreaterThan(0);
+            RuleFor(x => x.LessonId)
+                .NotNull()
+                .GreaterThan(0);
+            RuleFor(x => x.LessonDate)
+                .NotNull()
+                .Must(x => BeAValidDate(x));
+            RuleFor(x => x.StudentMark)
+                .NotNull()
+                .GreaterThanOrEqualTo((sbyte)0)
+                .LessThanOrEqualTo((sbyte)100);
+            RuleFor(x => x.Comment)
+                .NotEmpty()
+                .MaximumLength(ValidationConstants.MaxLengthName);
+        }
+        private bool BeAValidDate(DateTime? date)
+        {
+            return !date.Equals(default(DateTime?));
+        }
+    }
+}
---
+++ CharlieBackend.Api/Validators/DashboardDTOValidators/StudentsClassbookDTOValidators/StudentVisitDtoValidator.cs
@@ -0,0 +1,35 @@
+﻿using CharlieBackend.Business.Helpers;
+using CharlieBackend.Core.DTO.Dashboard;
+using FluentValidation;
+using System;
+
+namespace CharlieBackend.Api.Validators.DashboardDTOValidators.StudentsClassbookDTOValidators
+{
+    public class StudentVisitDtoValidator : AbstractValidator<StudentVisitDto>
+    {
+        public StudentVisitDtoValidator()
+        {
+            RuleFor(x => x.CourseId)
+              .NotNull()
+              .GreaterThan(0);
+            RuleFor(x => x.StudentGroupId)
+                .NotNull()
+                .GreaterThan(0);
+            RuleFor(x => x.StudentId)
+              .NotNull()
+              .GreaterThan(0);
+            RuleFor(x => x.LessonId)
+                .NotNull()
+                .GreaterThan(0);
+            RuleFor(x => x.LessonDate)
+                .NotNull()
+                .Must(x => BeAValidDate(x));
+            RuleFor(x => x.Presence)
+                .NotNull();
+        }
+        private bool BeAValidDate(DateTime? date)
+        {
+            return !date.Equals(default(DateTime?));
+        }
+    }
+}
