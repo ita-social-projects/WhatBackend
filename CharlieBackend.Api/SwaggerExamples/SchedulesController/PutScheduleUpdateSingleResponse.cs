983de4b8caaa4f8994934d749d42d7ebb4917d2b
Porti <Pasha19682000d@gmail.com>
2021-06-11 16:21:04 +0300

    fixed IncorrectExample

---
+++ CharlieBackend.Api/SwaggerExamples/SchedulesController/PutScheduleUpdateSingleResponse.cs
@@ -0,0 +1,40 @@
+﻿using CharlieBackend.Core.DTO.Schedule;
+using CharlieBackend.Core.Entities;
+using Swashbuckle.AspNetCore.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CharlieBackend.Api.SwaggerExamples.SchedulesController
+{
+    public class PutScheduleUpdateSingleResponse : IExamplesProvider<EventOccurrenceDTO>
+    {
+        public EventOccurrenceDTO GetExamples()
+        {
+            return new EventOccurrenceDTO()
+            {
+                Id = 0,
+                StudentGroupId= 1,
+                EventStart = DateTime.Now,
+                EventFinish = DateTime.Now,
+                Pattern = PatternType.RelativeMonthly,
+                Events = new List<ScheduledEventDTO> 
+                {
+                   new ScheduledEventDTO
+                   {
+                        Id = 1,
+                        EventOccuranceId = 1,
+                        StudentGroupId = 1,
+                        ThemeId = 6,
+                        MentorId = 5,
+                        LessonId = null,
+                        EventStart = DateTime.Now,
+                        EventFinish = DateTime.Now
+                   }
+                },
+                Storage = 30368744178210
+            };
+        }
+    }
+}
