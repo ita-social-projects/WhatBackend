bde5c2370b006bd4b4a6628b8894431c43319e47
Dmitry Krukovsky <megabelka1@gmail.com>
2021-06-14 21:04:49 +0300

    HomeworkStudentDTOValidators

--- CharlieBackend.Business/Helpers/ValidationConstants.cs
+++ CharlieBackend.Business/Helpers/ValidationConstants.cs
@@ -7,10 +7,13 @@ namespace CharlieBackend.Business.Helpers
         public const int MaxLengthPassword = 30;
         public const int MaxLengthEmail = 50;
         public const int MaxLengthName = 30;
+        public const int MaxLengthHeader = 100;
         public const int MaxLengthURL = 200;
+        public const int MaxLengthTaskText = 1024; // Is not specified anywhere.
+        public const int MaxLengthCommentText = 1024;
         public const string PasswordRule = "Password must have at least eight characters, at least one uppercase letter, one lowercase letter and one number";
         public const string PasswordConfirmNotValid = "Passwords do not match";
-        public const string DatesNotValid = "StartDate can not be greater than FinisDate";
+        public const string DatesNotValid = "StartDate can not be greater than FinishDate";
 
     }
 }
