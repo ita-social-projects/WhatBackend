9e1cbcbec3a18d9f4c13f9637b238bba91aef40d
Vso1o <vsolo1234@gmail.com>
2021-06-24 05:12:11 +0300

    First version - added controller

---
+++ CharlieBackend.AdminPanel/Controllers/EventOccurrenceController.cs
@@ -0,0 +1,12 @@
+﻿using CharlieBackend.AdminPanel.Services.Interfaces;
+using CharlieBackend.Core.DTO.Schedule;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CharlieBackend.AdminPanel.Controllers
+{
+    public class EventOccurrenceController : Controller
+    {
+    }
+}
