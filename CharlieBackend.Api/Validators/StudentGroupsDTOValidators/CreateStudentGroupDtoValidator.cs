e7f97ce287228f56f1fd3dfd82826319cc7446c7
Dmitry Krukovsky <megabelka1@gmail.com>
2021-07-02 13:23:11 +0300

    StudentGroups DTO Validators

---
+++ CharlieBackend.Api/Validators/StudentGroupsDTOValidators/CreateStudentGroupDtoValidator.cs
@@ -0,0 +1,35 @@
+﻿using CharlieBackend.Business.Helpers;
+using CharlieBackend.Core.DTO.StudentGroups;
+using FluentValidation;
+
+namespace CharlieBackend.Api.Validators.StudentGroupsDTOValidators
+{
+    public class CreateStudentGroupDtoValidator : AbstractValidator<CreateStudentGroupDto>
+    {
+        public CreateStudentGroupDtoValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .MaximumLength(ValidationConstants.MaxLengthName);
+            RuleFor(x => x.CourseId)
+                .NotEmpty()
+                .GreaterThan(0);
+            RuleFor(x => x.StartDate)
+               .NotEmpty();
+            RuleFor(x => x.FinishDate)
+                .Must((x, cancellation) => (x.FinishDate > x.StartDate || x.FinishDate.Equals(x.StartDate)))
+                .When(x => x.FinishDate != null)
+                .WithMessage(ValidationConstants.DatesNotValid);
+            RuleFor(x => x.StudentIds)
+                .NotEmpty();
+            RuleForEach(x => x.StudentIds)
+                .NotEmpty()
+                .GreaterThan(0);
+            RuleFor(x => x.MentorIds)
+                .NotEmpty();
+            RuleForEach(x => x.MentorIds)
+                .NotEmpty()
+                .GreaterThan(0);
+        }
+    }
+}
---
+++ CharlieBackend.Api/Validators/StudentGroupsDTOValidators/StudentGroupDtoValidator.cs
@@ -0,0 +1,31 @@
+﻿using CharlieBackend.Business.Helpers;
+using CharlieBackend.Core.DTO.StudentGroups;
+using FluentValidation;
+
+namespace CharlieBackend.Api.Validators.StudentGroupsDTOValidators
+{
+    public class StudentGroupDtoValidator : AbstractValidator<StudentGroupDto>
+    {
+        public StudentGroupDtoValidator()
+        {
+            RuleFor(x => x.Id)
+               .NotEmpty()
+               .GreaterThan(0);
+            RuleFor(x => x.CourseId)
+                .NotEmpty()
+                .GreaterThan(0);
+            RuleFor(x => x.Name)
+                .MaximumLength(ValidationConstants.MaxLengthHeader);
+            RuleFor(x => x.FinishDate)
+                .Must((x, cancellation) => (x.FinishDate > x.StartDate || x.FinishDate.Equals(x.StartDate)))
+                .When(x => x.FinishDate != null)
+                .WithMessage(ValidationConstants.DatesNotValid);
+            RuleForEach(x => x.StudentIds)
+                .NotEmpty()
+                .GreaterThan(0);
+            RuleForEach(x => x.MentorIds)
+                .NotEmpty()
+                .GreaterThan(0);
+        }
+    }
+}
