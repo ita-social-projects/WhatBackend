f93bb4fd5cc156477324d23c8c4412613a8416eb
Dmitry Krukovsky <megabelka1@gmail.com>
2021-07-02 13:24:37 +0300

    Visit DTO Validator

---
+++ CharlieBackend.Api/Validators/VisitDTOValidators/VisitDtoValidator.cs
@@ -0,0 +1,25 @@
+﻿using CharlieBackend.Business.Helpers;
+using CharlieBackend.Core.DTO.Visit;
+using FluentValidation;
+
+namespace CharlieBackend.Api.Validators.VisitDTOValidators
+{
+    public class VisitDtoValidator : AbstractValidator<VisitDto>
+    {
+        public VisitDtoValidator()
+        {
+            RuleFor(x => x.StudentId)
+                .NotEmpty()
+                .GreaterThan(0);
+            RuleFor(x => x.StudentMark)
+                .NotEmpty()
+                .GreaterThanOrEqualTo((sbyte)0);
+            RuleFor(x => x.Presence)
+                .NotNull();
+            RuleFor(x => x.Comment)
+                .NotEmpty()
+                .MaximumLength(ValidationConstants.MaxLengthCommentText);
+
+        }
+    }
+}
