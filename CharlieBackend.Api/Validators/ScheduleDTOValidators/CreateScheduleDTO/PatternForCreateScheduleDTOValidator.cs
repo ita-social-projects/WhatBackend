e5695518c245dafe55c7ed06bd9e96d0868ad435
Dmitry Krukovsky <megabelka1@gmail.com>
2021-07-09 01:33:28 +0300

    PatternForCreateScheduleDTO Validator change + tests

--- CharlieBackend.Api/Validators/ScheduleDTOValidators/CreateScheduleDTO/PatternForCreateScheduleDTOValidator.cs
+++ CharlieBackend.Api/Validators/ScheduleDTOValidators/CreateScheduleDTO/PatternForCreateScheduleDTOValidator.cs
@@ -8,12 +8,12 @@ namespace CharlieBackend.Api.Validators.Schedule.CreateScheduleDTO
         public PatternForCreateScheduleDTOValidator()
         {
             RuleFor(x => x.Type)
-                .NotEmpty();
+                .NotNull();
             RuleFor(x => x.Interval)
                 .NotEmpty()
                 .GreaterThan(0);
             RuleForEach(x => x.DaysOfWeek)
-                .NotEmpty();
+                .NotNull();
             RuleForEach(x => x.Dates)
                 .GreaterThanOrEqualTo(1)
                 .LessThanOrEqualTo(31);
