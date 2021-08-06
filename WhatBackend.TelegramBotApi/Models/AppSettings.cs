e4817199da36fb374dcb08ffc8464afd73d216d0
Dmitry Krukovsky <megabelka1@gmail.com>
2021-08-06 17:40:16 +0300

    Moved bot data to appsettings.json

--- WhatBackend.TelegramBotApi/Models/AppSettings.cs
+++ WhatBackend.TelegramBotApi/Models/AppSettings.cs
@@ -2,8 +2,16 @@
 {
     public class AppSettings
     {
-        public static string Url { get; set; } = "https://ee4d50b27a67.ngrok.io";
-        public static string Key { get; set; } = "1945675656:AAF5rVHswcwa8-xIZLS4DUlgFL2RW8mhrGM";
-        public static string Name { get; set; } = "whatnotification_bot";
+        public static string Url { get; set; } 
+        public static string Key { get; set; }
+        public static string Name { get; set; }
+
+        public static void GetData(string url, string key, string name)
+        {
+            Url = url;
+            Key = key;
+            Name = name;
+        }
     }
+
 }
