8c1f9297a3d049bf0fcab0b0a995e2049c18ef9f
Vso1o <vsolo1234@gmail.com>
2021-08-17 15:35:02 +0300

    Xml documentation for BaseFileExport

--- CharlieBackend.Business/Services/FileServices/ExportFileServices/BaseFileExport.cs
+++ CharlieBackend.Business/Services/FileServices/ExportFileServices/BaseFileExport.cs
@@ -22,13 +22,6 @@ namespace CharlieBackend.Business.Services.FileServices.ExportFileServices
             memoryStream = new MemoryStream();
         }
 
-        public async Task<Result<IXLRow>> CreateHeadersAsync(IXLRow headerRow, params string[] argsList)
-        {
-            var result = await Task.Run(() => CreateHeaders(headerRow, argsList));
-
-            return Result<IXLRow>.GetSuccess(result);
-        }
-
         private IXLRow CreateHeaders(IXLRow headerRow, params string[] argsList)
         {
             for (int header = 0; header < argsList.Length; header++)
@@ -37,11 +30,6 @@ namespace CharlieBackend.Business.Services.FileServices.ExportFileServices
             }
 
             return headerRow;
-        }
-
-        public async Task<byte[]> GetByteArrayAsync()
-        {
-            return await Task.Run(()=> GetByteArray());
         }
 
         private byte[] GetByteArray()
@@ -54,16 +42,54 @@ namespace CharlieBackend.Business.Services.FileServices.ExportFileServices
             return memoryStream.ToArray();
         }
 
+        /// <summary>
+        /// Fills row with headers starting from first column (can be used for other rows as well)
+        /// </summary>
+        /// <param name="headerRow">Row which has to be filled</param>
+        /// <param name="argsList">Variable number of params which will be inserted into mentioned row</param>
+        /// <returns>Ref to the row has been filled</returns>
+        public async Task<Result<IXLRow>> CreateHeadersAsync(IXLRow headerRow, params string[] argsList)
+        {
+            var result = await Task.Run(() => CreateHeaders(headerRow, argsList));
+
+            return Result<IXLRow>.GetSuccess(result);
+        }
+
+        /// <summary>
+        /// Creates byte arrray from file (Should be used to create and return file)
+        /// </summary>
+        /// <returns>Byte arrray</returns>
+        public async Task<byte[]> GetByteArrayAsync()
+        {
+            return await Task.Run(()=> GetByteArray());
+        }
+
+        /// <summary>
+        /// Returns file name and has to be overrided by inheritors to set specified file name
+        /// </summary>
+        /// <returns>
+        /// (String) file name
+        /// </returns>
         public virtual string GetFileName()
         {
             return "Filename_"+ DateTime.Now.ToString("yyyy-MM-dd") +".xlsx";
         }
 
+        /// <summary>
+        /// Returns content type (Should be used to create and return file)
+        /// </summary>
+        /// <returns>
+        /// application/octet-stream
+        /// </returns>
         public string GetContentType()
         {
             return "application/octet-stream";
         }
 
+        /// <summary>
+        /// Draws borders for specified range (Medium outer borders and thin inner borders)
+        /// </summary>
+        /// <param name="targetRange">Range which needs to get borders</param>
         public void DrawBorders(IXLRange targetRange)
         {
             targetRange.Style
@@ -75,6 +101,10 @@ namespace CharlieBackend.Business.Services.FileServices.ExportFileServices
                 .InsideBorder = XLBorderStyleValues.Thin;
         }
 
+        /// <summary>
+        /// Ajusts content for all sheets in excel book 
+        /// (Sets width and height to avoid hiding part of words etc.)
+        /// </summary>
         public void AdjustContent()
         {
             foreach (var worksheet in xLWorkbook.Worksheets)
@@ -85,6 +115,16 @@ namespace CharlieBackend.Business.Services.FileServices.ExportFileServices
             
         }
 
+        /// <summary>
+        /// Fills rows with parameters in argList
+        /// </summary>
+        /// <param name="worksheet"> Worksheet which has to be filled </param>
+        /// <param name="rowNumber"> id of row which has to be filled </param>
+        /// <param name="startingColumnNumber"> id of column from where cells would be filled </param>
+        /// <param name="argsList"> 
+        /// List of parameters which will be inserted 
+        /// If you need to skip some cells use "" or " "
+        /// </param>
         public void FillRow(IXLWorksheet worksheet, int rowNumber = 1, int startingColumnNumber = 1, params string[] argsList)
         {
             var currentRow = worksheet.Row(rowNumber);
@@ -95,6 +135,17 @@ namespace CharlieBackend.Business.Services.FileServices.ExportFileServices
             }
         }
 
+        /// <summary>
+        /// Fills rows with parameters in argList as comments
+        /// </summary>
+        /// <param name="worksheet"> Worksheet which has to be filled </param>
+        /// <param name="rowNumber"> id of row which has to be filled </param>
+        /// <param name="startingColumnNumber"> id of column from where cells would be filled </param>
+        /// <param name="argsList"> 
+        /// List of parameters which will be inserted 
+        /// nulls will be ignored
+        /// If you want to avoid empty comments replace all "" with null before calling this method
+        /// </param>
         public void FillRowWithComments(IXLWorksheet worksheet, int rowNumber = 1, int startingColumnNumber = 1, params string[] argsList)
         {
             var currentRow = worksheet.Row(rowNumber);
