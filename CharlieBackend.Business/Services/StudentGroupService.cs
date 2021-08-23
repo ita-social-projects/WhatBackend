d4aa8442e21782148331a97dba34e791925b6810
Ilhamich <yasniy89@gmail.com>
2021-08-23 18:21:35 +0300

    Was did refactoring for  CreateStudentGroupAsync and fix IsStudenHisOwnMentor

--- CharlieBackend.Business/Services/StudentGroupService.cs
+++ CharlieBackend.Business/Services/StudentGroupService.cs
@@ -19,55 +19,24 @@ namespace CharlieBackend.Business.Services
         private readonly IMapper _mapper;
         private readonly ILogger<StudentGroupService> _logger;
 
-        public StudentGroupService(IUnitOfWork unitOfWork, IMapper mapper, ILogger<StudentGroupService> logger)
+        public StudentGroupService(IUnitOfWork unitOfWork, IMapper mapper,
+                ILogger<StudentGroupService> logger)
         {
             _unitOfWork = unitOfWork;
             _mapper = mapper;
             _logger = logger;
         }
 
-        public async Task<Result<StudentGroupDto>> CreateStudentGroupAsync(CreateStudentGroupDto studentGroupDto)
+        public async Task<Result<StudentGroupDto>> CreateStudentGroupAsync
+                (CreateStudentGroupDto studentGroupDto)
         {
             try
             {
-                if (studentGroupDto == null)
+                var result = await CheckCreateStudentGroupDto(studentGroupDto);
+
+                if (result.Error != null)
                 {
-                    return Result<StudentGroupDto>.GetError(ErrorCode.ValidationError, "StudentGroupDto is null");
-                }
-
-                if (await _unitOfWork.StudentGroupRepository.IsGroupNameExistAsync(studentGroupDto.Name))
-                {
-                    return Result<StudentGroupDto>.GetError(ErrorCode.UnprocessableEntity, "Group name already exists");
-                }
-
-                if (!await _unitOfWork.CourseRepository.IsEntityExistAsync(studentGroupDto.CourseId))
-                {
-                    return Result<StudentGroupDto>.GetError(ErrorCode.ValidationError, "CourseId does not exist");
-                }
-
-                if (studentGroupDto.StudentIds != null)
-                {
-                    var dublicatesStudent = studentGroupDto.StudentIds.Dublicates();
-
-                    if (dublicatesStudent.Any())
-                    {
-                        return Result<StudentGroupDto>.GetError(ErrorCode.ValidationError, $"Such student ids: {string.Join(" ", dublicatesStudent)} are not unique");
-                    }
-                }
-
-                if (studentGroupDto.MentorIds != null)
-                {
-                    var dublicatesMentor = studentGroupDto.MentorIds.Dublicates();
-
-                    if (dublicatesMentor.Any())
-                    {
-                        return Result<StudentGroupDto>.GetError(ErrorCode.ValidationError, $"Such mentor ids: {string.Join(" ", dublicatesMentor)} are not unique");
-                    }
-                }
-
-                if (studentGroupDto.StartDate > studentGroupDto.FinishDate)
-                {
-                    return Result<StudentGroupDto>.GetError(ErrorCode.ValidationError, "Start date must be less than finish date");
+                    return result;
                 }
 
                 var studentGroup = new StudentGroup
@@ -82,81 +51,161 @@ namespace CharlieBackend.Business.Services
 
                 if (studentGroupDto.StudentIds?.Count > 0)
                 {
-                    var notExistStudentIds = await _unitOfWork.StudentRepository.GetNotExistEntitiesIdsAsync(studentGroupDto.StudentIds);
+                    var notExistStudentIds = await _unitOfWork.StudentRepository.
+                            GetNotExistEntitiesIdsAsync(
+                                    studentGroupDto.StudentIds);
 
                     if (notExistStudentIds.Any())
                     {
-                        return Result<StudentGroupDto>.GetError(errorCode: ErrorCode.ValidationError,
-                                                                errorMessage: GenerateErrorMessage("Student", notExistStudentIds));
+                        return Result<StudentGroupDto>.GetError(
+                                ErrorCode.ValidationError,
+                                GenerateErrorMessage("Student",
+                                        notExistStudentIds));
                     }
 
-                    var students = await _unitOfWork.StudentRepository.GetStudentsByIdsAsync(studentGroupDto.StudentIds);
-                    studentGroup.StudentsOfStudentGroups = new List<StudentOfStudentGroup>();
+                    var students = await _unitOfWork.StudentRepository.
+                            GetStudentsByIdsAsync(studentGroupDto.StudentIds);
 
-                    for (int i = 0; i < students.Count; i++)
+                    studentGroup.StudentsOfStudentGroups =
+                            new List<StudentOfStudentGroup>();
+
+                    foreach (var s in students)
                     {
-                        studentGroup.StudentsOfStudentGroups.Add(new StudentOfStudentGroup
-                        {
-                            StudentId = students[i].Id,
-                            Student = students[i]
-                        });
+                        studentGroup.StudentsOfStudentGroups.Add(
+                                new StudentOfStudentGroup
+                                { StudentId = s.Id, Student = s });
                     }
                 }
 
                 if (studentGroupDto.MentorIds?.Count > 0)
                 {
-                    var notExistMentorsIds = await _unitOfWork.MentorRepository.GetNotExistEntitiesIdsAsync(studentGroupDto.MentorIds);
+                    var notExistMentorsIds = await _unitOfWork.MentorRepository.
+                            GetNotExistEntitiesIdsAsync(
+                                    studentGroupDto.MentorIds);
 
                     if (notExistMentorsIds.Any())
                     {
-                        return Result<StudentGroupDto>.GetError(errorCode: ErrorCode.ValidationError,
-                                                                errorMessage: GenerateErrorMessage("Mentor", notExistMentorsIds));
+                        return Result<StudentGroupDto>.GetError(
+                                ErrorCode.ValidationError,
+                                GenerateErrorMessage("Mentor",
+                                        notExistMentorsIds));
                     }
 
-                    var mentors = await _unitOfWork.MentorRepository.GetMentorsByIdsAsync(studentGroupDto.MentorIds);
-                    studentGroup.MentorsOfStudentGroups = new List<MentorOfStudentGroup>();
+                    var mentors = await _unitOfWork.MentorRepository.
+                            GetMentorsByIdsAsync(studentGroupDto.MentorIds);
 
-                    for (int i = 0; i < mentors.Count; i++)
+                    studentGroup.MentorsOfStudentGroups =
+                            new List<MentorOfStudentGroup>();
+
+                    foreach (var m in mentors)
                     {
-                        studentGroup.MentorsOfStudentGroups.Add(new MentorOfStudentGroup
-                        {
-                            MentorId = mentors[i].Id,
-                            Mentor = mentors[i]
-                        }); ;
+                        studentGroup.MentorsOfStudentGroups.Add(
+                                new MentorOfStudentGroup
+                                { MentorId = m.Id, Mentor = m });
                     }
                 }
 
-                var isStudentMentor = await IsStudenHisOwnMentor(studentGroup);
+                var studentId = await IsStudentHisOwnMentor(studentGroup);
 
-                if (isStudentMentor != null)
+                if (studentId != null)
                 {
                     return Result<StudentGroupDto>.GetError(
-                             ErrorCode.Conflict, "The student id " +
-                             $"{isStudentMentor} is his own mentor");
+                             ErrorCode.Conflict, "The student accountId " +
+                             $"{studentId} is his own mentor");
                 }
 
                 await _unitOfWork.CommitAsync();
 
-                return Result<StudentGroupDto>.GetSuccess(_mapper.Map<StudentGroupDto>(studentGroup));
+                return Result<StudentGroupDto>.GetSuccess(
+                            _mapper.Map<StudentGroupDto>(studentGroup));
             }
             catch
             {
                 _unitOfWork.Rollback();
 
-                return Result<StudentGroupDto>.GetError(ErrorCode.InternalServerError, "Internal error");
+                return Result<StudentGroupDto>.GetError(
+                            ErrorCode.InternalServerError, "Internal error");
             }
         }
 
-        private Task<long?> IsStudenHisOwnMentor(StudentGroup group)
+        private async Task<Result<StudentGroupDto>> CheckCreateStudentGroupDto(
+             CreateStudentGroupDto studentGroupDto)
         {
-            var r = group.MentorsOfStudentGroups
+            if (studentGroupDto == null)
+            {
+                return Result<StudentGroupDto>.GetError(
+                        ErrorCode.ValidationError,
+                        "StudentGroupDto is null");
+            }
+
+            if (await _unitOfWork.StudentGroupRepository.IsGroupNameExistAsync(
+                        studentGroupDto.Name))
+            {
+                return Result<StudentGroupDto>.GetError(
+                        ErrorCode.UnprocessableEntity,
+                        "Group name already exists");
+            }
+
+            if (!await _unitOfWork.CourseRepository.IsEntityExistAsync(
+                    studentGroupDto.CourseId))
+            {
+                return Result<StudentGroupDto>.GetError(
+                        ErrorCode.ValidationError,
+                        "CourseId does not exist");
+            }
+
+            if (studentGroupDto.StartDate > studentGroupDto.FinishDate)
+            {
+                return Result<StudentGroupDto>.GetError(
+                        ErrorCode.ValidationError,
+                        "Start date must be less than finish date");
+            }
+
+            var result = await CheckDublicate(studentGroupDto.StudentIds);
+
+            if (result.Error == null)
+            {
+                result = await CheckDublicate(studentGroupDto.MentorIds);
+            }
+
+            return result;
+        }
+
+        private Task<Result<StudentGroupDto>> CheckDublicate(IList<long> list)
+        {
+            Result<StudentGroupDto> result = new Result<StudentGroupDto>();
+
+            if (list != null)
+            {
+                var dublicates = list.Dublicates();
+
+                if (dublicates.Any())
+                {
+                    result = Result<StudentGroupDto>.GetError(
+                            ErrorCode.ValidationError, $"Such ids: " +
+                            $"{string.Join(' ', dublicates)} are not unique");
+                }
+            }
+
+            return Task.FromResult(result);
+        }
+
+        private Task<long?> IsStudentHisOwnMentor(StudentGroup group)
+        {
+            if (group.MentorsOfStudentGroups == null
+                        || group.StudentsOfStudentGroups == null)
+            {
+                long? result = null;
+
+                return Task.FromResult(result);
+            }
+
+            return Task.FromResult(group.MentorsOfStudentGroups
                     .Select(m => m.Mentor.AccountId)
                     .Where(id => id != null)
                     .FirstOrDefault(id => group.StudentsOfStudentGroups
                             .Where(s => s.Student.AccountId != null)
-                            .Any(s => s.Student.AccountId == id));
-                    
-            return Task.FromResult(r);
+                            .Any(s => s.Student.AccountId == id)));
         }
 
         public async Task<Result<bool>> IsGroupNameExistAsync(string name)
