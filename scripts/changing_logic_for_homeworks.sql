8b2f9494c9fc2005650fddf0607335488807bec7
Tkachelnik <72625589+Tkachelnik@users.noreply.github.com>
2021-08-25 13:22:33 +0300

    Update changing_logic_for_homeworks.sql

--- scripts/changing_logic_for_homeworks.sql
+++ scripts/changing_logic_for_homeworks.sql
@@ -2,26 +2,26 @@ USE `Soft`;
 
 CREATE TABLE `Marks` (
     `ID`                BIGINT UNSIGNED     NOT NULL         AUTO_INCREMENT,
-	`Value`             TINYINT             DEFAULT NULL,        
+    `Value`             TINYINT             DEFAULT NULL,        
     `Comment`           VARCHAR(1024)       DEFAULT NULL,
     `EvaluationDate`    DATETIME            NOT NULL,
     `Type`              TINYINT UNSIGNED    NOT NULL         COMMENT 'Types:\n 0 - Homework,\n 1 - Visit',
-    `EvaluatedBy`		BIGINT UNSIGNED    	NOT NULL,
+    `EvaluatedBy`	BIGINT UNSIGNED     NOT NULL,
 	
     CONSTRAINT    `PK_Mark`             PRIMARY KEY (`ID`),
     CONSTRAINT    `FK_AccountOfMark`    FOREIGN KEY (`EvaluatedBy`)    REFERENCES `Accounts` (`ID`)
 );
 
 ALTER TABLE `HomeworksFromStudents` 
-    ADD    `MarkID` 		   BIGINT UNSIGNED    DEFAULT NULL,
+    ADD    `MarkID` 	       BIGINT UNSIGNED    DEFAULT NULL,
     ADD    `PublishingDate`    DATETIME           NOT NUll,
-    ADD    `IsSent`			   BOOLEAN 	          NOT NULL,
+    ADD    `IsSent`	       BOOLEAN 	          NOT NULL,
     
     ADD CONSTRAINT    `FK_MarkOfHomeworkFromStudent`    FOREIGN KEY (`MarkID`)    REFERENCES `Marks` (`ID`);
   
 ALTER TABLE `Homeworks`
     ADD    `PublishingDate`    DATETIME           NOT NULL,
-    ADD    `CreatedBy`   	   BIGINT UNSIGNED    NOT NULL,
+    ADD    `CreatedBy`         BIGINT UNSIGNED    NOT NULL,
     
     ADD CONSTRAINT    `FK_AccountOfHomework`    FOREIGN KEY (`CreatedBy`)    REFERENCES `Accounts` (`ID`);
     
