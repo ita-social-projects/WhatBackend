c9ef8089c7bff585a2a7fa170f498c2a06452dcb
Dmitry Krukovsky <megabelka1@gmail.com>
2021-09-07 16:53:24 +0300

    Mark command stub

--- WhatBackend.TelegramBotApi/Models/Commands/GetMarkCommand.cs
+++ WhatBackend.TelegramBotApi/Models/Commands/GetMarkCommand.cs
@@ -4,10 +4,14 @@ using Telegram.Bot;
 using Telegram.Bot.Types;
 using CharlieBackend.Core.DTO.Dashboard;
 using CharlieBackend.Business.Services.Interfaces;
-
+using CharlieBackend.Core.Models.ResultModel;
 
 namespace WhatBackend.TelergamBot.Models.Commands
 {
+
+    /// <summary>
+    /// This command uses UserID to Send him message about his recent marks
+    /// </summary>
     public class GetMarkCommand : Command
     {
         private readonly IDashboardService _dashboardService;
@@ -21,18 +25,32 @@ namespace WhatBackend.TelergamBot.Models.Commands
             var chatId = message.Chat.Id;
             var messageId = message.MessageId;
 
-            var userId = 1;
-            var request = new DashboardAnalyticsRequestDto<StudentResultType> {
-                StartDate = default(DateTime),
-                FinishDate = default(DateTime),
-                IncludeAnalytics = new StudentResultType[0]
+            //var userId = 1;
+            //var request = new DashboardAnalyticsRequestDto<StudentResultType>
+            //{
+            //    StartDate = default(DateTime),
+            //    FinishDate = default(DateTime),
+            //    IncludeAnalytics = new StudentResultType[0]
+            //};
+            //var results = await _dashboardService.GetStudentResultAsync(userId, request);
+
+            var results = new Result<StudentMarkDto>  //temporary stub to make this work
+            {
+                Data = new StudentMarkDto
+                {
+                    StudentGroupId = 1,
+                    StudentId = 1,
+                    LessonId = 1,
+                    CourseId = 1,
+                    StudentMark = 10
+                },
+                Error = null
             };
 
-            var results = await _dashboardService.GetStudentResultAsync(userId, request);
+            var recentMark = results.Data.StudentMark;
+            var replyMessage = string.Format("Your recent mark is {0}", recentMark.ToString());
+            await client.SendTextMessageAsync(chatId, replyMessage, replyToMessageId: messageId);
 
-            var recentMarks = results.Data.AverageStudentsMarks;
-
-            await client.SendTextMessageAsync(chatId, recentMarks.ToString(), replyToMessageId: messageId);
         }
     }
 }
