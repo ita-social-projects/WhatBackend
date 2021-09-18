8a3d823fcc34f6b101eca2c559b717f7c97f768f
Ilhamich <yasniy89@gmail.com>
2021-09-18 13:00:41 +0300

    Changed StudentGroupXlsxFileImporter and created base StudentGroupFileImporter

--- CharlieBackend.Business/Services/FileServices/ImportFileServices/Xlsx/StudentsGroupXlsxFileImporter.cs
+++ CharlieBackend.Business/Services/FileServices/ImportFileServices/Xlsx/StudentsGroupXlsxFileImporter.cs
@@ -1,105 +1,76 @@
 ﻿using CharlieBackend.Business.Helpers;
+using CharlieBackend.Business.Services.FileServices.ImportFileServices.Base;
 using CharlieBackend.Business.Services.Interfaces;
 using CharlieBackend.Core.DTO.Account;
-using CharlieBackend.Core.DTO.Student;
-using CharlieBackend.Core.DTO.StudentGroups;
 using CharlieBackend.Core.Models.ResultModel;
-using CharlieBackend.Data.Repositories.Impl.Interfaces;
 using ClosedXML.Excel;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace CharlieBackend.Business.Services.FileServices
+namespace CharlieBackend.Business.Services.FileServices.ImportFileServices.Xlsx
 {
-    public class StudentsGroupXlsxFileImporter
+    public class StudentsGroupXlsxFileImporter : StudentGroupFileImporter
     {
-        private readonly IStudentGroupService _studentGroupService;
-        private readonly IStudentService _studentService;
-        private readonly IUnitOfWork _unitOfWork;
         private readonly IAccountService _accountService;
 
         public StudentsGroupXlsxFileImporter(
-            IStudentGroupService studentGroupService,
-            IStudentService studentService,
-            IAccountService accountService,
-            IUnitOfWork unitOfWork)
+                IStudentGroupService studentGroupService,
+                IStudentService studentService,
+                IAccountService accountService)
+                : base(studentService, studentGroupService)
         {
-            _studentGroupService = studentGroupService;
-            _studentService = studentService;
             _accountService = accountService;
-            _unitOfWork = unitOfWork;
         }
 
-        public async Task<Result<GroupWithStudentsDto>> ImportGroupAsync(
-                CreateStudentGroupDto studentGroup, string filePath)
+        protected override async Task<Result<IList<AccountDto>>> СollectAccountsFromFilePath(
+                    string filePath)
         {
-            var students = new List<StudentDto>();
-            var detailedGroup = new GroupWithStudentsDto();
+            var accountsForStudents = new List<AccountDto>();
 
             using (IXLWorkbook book = new XLWorkbook(filePath))
             {
-                #region group_creation
-                var group = await _studentGroupService.CreateStudentGroupAsync(new CreateStudentGroupDto
-                {
-                    CourseId = studentGroup.CourseId,
-                    Name = book.Worksheet(1).Row(2).Cell((int)StudentsGroupWorksheetHeader.GroupName).GetValue<string>(),
-                    StartDate = Convert.ToDateTime(book.Worksheet(1).Row(2).Cell((int)StudentsGroupWorksheetHeader.StartDate).GetValue<string>()),
-                    FinishDate = Convert.ToDateTime(book.Worksheet(1).Row(2).Cell((int)StudentsGroupWorksheetHeader.FinishDate).GetValue<string>()),
-                });
-
-                if (group.Error != null)
-                {
-                    return Result<GroupWithStudentsDto>.GetError(group.Error.Code, group.Error.Message);
-                }
-
-                detailedGroup = new GroupWithStudentsDto
-                {
-                    Id = group.Data.Id,
-                    CourseId = group.Data.CourseId,
-                    Name = group.Data.Name,
-                    StartDate = group.Data.StartDate,
-                    FinishDate = group.Data.FinishDate
-                };
-                #endregion
-
                 foreach (IXLRow row in book.Worksheet(1).RowsUsed().Skip(1))
                 {
-                    #region account_creation
-                    var _tempPassword = PasswordHelper.GeneratePassword();
-
-                    var account = await _accountService.CreateAccountAsync(new CreateAccountDto
+                    var account = new CreateAccountDto
                     {
                         Email = row.Cell((int)StudentsGroupWorksheetHeader.StudentEmail).GetValue<string>(),
                         FirstName = row.Cell((int)StudentsGroupWorksheetHeader.StudentFirstName).GetValue<string>(),
                         LastName = row.Cell((int)StudentsGroupWorksheetHeader.StudentLastName).GetValue<string>(),
-                        Password = _tempPassword,
-                        ConfirmPassword = _tempPassword
-                    });
+                    };
 
-                    if (account.Error != null)
+                    if (await _accountService.IsEmailTakenAsync(account.Email))
                     {
-                        return Result<GroupWithStudentsDto>.GetError(account.Error.Code, account.Error.Message);
+                        var accountSearch = await _accountService
+                                .GetAccountCredentialsByEmailAsync(account.Email);
+
+                        accountsForStudents.Add(accountSearch.Data);
                     }
-                    #endregion
-
-                    var student = await _studentService.CreateStudentAsync(account.Data.Id);
-
-                    if (student.Error != null)
+                    else
                     {
-                        return Result<GroupWithStudentsDto>.GetError(student.Error.Code, student.Error.Message);
-                    }
+                        var _tempPassword = PasswordHelper.GeneratePassword();
 
-                    students.Add(student.Data);
+                        account.Password = _tempPassword;
+                        account.ConfirmPassword = _tempPassword;
+
+                        var accountCreating = await _accountService
+                            .CreateAccountAsync(account);
+
+                        if (accountCreating.Error != null)
+                        {
+                            return Result<IList<AccountDto>>.GetError(
+                                    accountCreating.Error.Code,
+                                    accountCreating.Error.Message);
+                        }
+                        else
+                        {
+                            accountsForStudents.Add(accountCreating.Data);
+                        }
+                    }
                 }
-
-                detailedGroup.Students = students;
             }
 
-            await _unitOfWork.CommitAsync();
-
-            return Result<GroupWithStudentsDto>.GetSuccess(detailedGroup);
+            return Result<IList<AccountDto>>.GetSuccess(accountsForStudents);
         }
     }
 }
