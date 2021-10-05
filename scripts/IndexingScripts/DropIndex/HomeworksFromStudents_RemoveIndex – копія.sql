ee8f28e4c595a63fec5511c8fc2343eb81188f71
Vso1o <vsolo1234@gmail.com>
2021-10-05 13:54:56 +0300

    Removed comments in scripts

--- scripts/IndexingScripts/DropIndex/HomeworksFromStudents_RemoveIndex – копія.sql
+++ scripts/IndexingScripts/DropIndex/HomeworksFromStudents_RemoveIndex – копія.sql
@@ -1,8 +1,3 @@
 USE soft;
 
-/*ALTER TABLE homeworksfromstudents DROP FOREIGN KEY FK_StudentOfHomeworks;*/
-
 DROP INDEX marksIndex ON homeworksfromstudents;
-
-/*ALTER TABLE homeworksfromstudents ADD CONSTRAINT    `FK_StudentOfHomeworks`    FOREIGN KEY (`StudentID`)	REFERENCES `Students` (`ID`);
-*/
