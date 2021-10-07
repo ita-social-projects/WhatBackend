133fcbaedddb6a4b349764c61e481c5a7ee77a9f
Alexandr Tkachuk <sasha2812tk@gmail.com>
2021-10-07 23:42:51 +0300

    Add HangfireActivator

---
+++ CharlieBackend.Business/Services/Notification/HangfireActivator.cs
@@ -0,0 +1,21 @@
+﻿using Hangfire;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace CharlieBackend.Business.Services.Notification
+{
+    public class HangfireActivator : JobActivator
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public HangfireActivator(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public override object ActivateJob(Type type)
+        {
+            return _serviceProvider.GetRequiredService(type);
+        }
+    }
+}
