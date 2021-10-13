edf7f7a76b2996ab1a4471e3f4fff61bba26ae96
Tkachelnik <72625589+Tkachelnik@users.noreply.github.com>
2021-10-13 19:03:26 +0300

    Update 11_add_table_jobmappings.sql

--- scripts/11_add_table_jobmappings.sql
+++ scripts/11_add_table_jobmappings.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS `JobMappings`;
 CREATE TABLE `JobMappings` (
 	`ID`               BIGINT UNSIGNED    NOT NULL    AUTO_INCREMENT,
 	`CustomJobID`	   VARCHAR(50)	      NOT NULL,    
-	`HangfireJobID`	   VARCHAR(1000)   	  NOT NULL,
+	`HangfireJobID`	   VARCHAR(1000)      NOT NULL,
     
 	CONSTRAINT    `PK_JobMappings`    PRIMARY KEY (`ID`)
 );
