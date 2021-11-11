380490eca52d085326b4f91df508ab59c7898988
Дмитро Фесак <Fesak.D.V@nmu.one>
2021-11-11 11:22:52 +0200

    removed comments

--- scripts/0010_add_LastEditorId.sql
+++ scripts/0010_add_LastEditorId.sql
@@ -1,12 +1,5 @@
 USE `Soft`;
 
-LOCK TABLES `Visits` WRITE;
-
-ALTER TABLE `Visits` 
-	ADD		`LastEditorID`		BIGINT UNSIGNED		NOT NULL		DEFAULT 1;
-
-UNLOCK TABLES;
-
 LOCK TABLES `Accounts` WRITE;
 
 ALTER TABLE `Accounts` 
