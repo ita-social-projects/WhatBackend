5d16ebdfb124c34106af936f8f1c7e19b780a1c2
leshikevich <34105791+leshikevich@users.noreply.github.com>
2022-01-26 16:29:05 +0200

    bug #744 NullReferenceException given when trying to click on Homeworks in Panel (logged in as Secretary) (#770)
    Access was add to the user with the "Secretary" role to the HomeWorks tab

--- CharlieBackend.Api/Controllers/HomeworksController.cs
+++ CharlieBackend.Api/Controllers/HomeworksController.cs
@@ -37,7 +37,7 @@ namespace CharlieBackend.Api.Controllers
         /// <returns>
         /// All Homewrok's entities
         /// </returns>
-        [Authorize(Roles = "Admin, Mentor")]
+        [Authorize(Roles = "Admin, Mentor, Secretary")]
         [HttpGet]
         [MapToApiVersion("2.0")]
         public async Task<ActionResult<IList<HomeworkDto>>> GetHomeworks()
