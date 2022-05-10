46b17afc7c7f1e7b5ac6890ea8cbe2d5fcae37b6
Bohdan Zaliubovskyi <bzali@softserveinc.com>
2022-05-10 14:20:55 +0300

    bugfix/866-addMinSizeToListTables added minimum size to all tables fixed homework table caption fixed courses caption the code was brought to one form in all data table views fixed positions of all counter divs

--- CharlieBackend.Panel/wwwroot/js/demo/datatables-demo.js
+++ CharlieBackend.Panel/wwwroot/js/demo/datatables-demo.js
@@ -4,4 +4,18 @@ $(document).ready(function() {
     $('#groupsDataTable').DataTable({
         "order": [[2, "desc"]]
     });
+
+    var counter = document.getElementById('dataTable_length');
+    if (counter != null) {
+        counter.style.float = 'left';
+    }
+    var counter = document.getElementById('groupsDataTable_length');
+    if (counter != null) {
+        counter.style.float = 'left';
+    }
+    
+    var content = document.getElementById('content');
+    if (content != null) {
+        content.style.minWidth = '900px';
+    }
 });
