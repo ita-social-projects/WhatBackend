92b013b7c6900fc3382697d0193a5152ebec2722
Andrey Mironov <dendyandy10@gmail.com>
2022-05-16 11:20:23 +0300

    remove public specifier from interface

--- CharlieBackend.Panel/Services/Interfaces/IExportService.cs
+++ CharlieBackend.Panel/Services/Interfaces/IExportService.cs
@@ -5,11 +5,11 @@ namespace CharlieBackend.Panel.Services.Interfaces
 {
     public interface IExportService
     {
-        public Task<byte[]> ExportStudentsClassbook(ExportFileFormat format, ExportByCourseAndStudentGroupModel exportModel);
-        public Task<byte[]> ExportStudentsResults(ExportFileFormat format, ExportByCourseAndStudentGroupModel exportModel);
-        public Task<byte[]> ExportStudentClassbook(ExportFileFormat format, long studentId, ExportByDateRangeModel exportModel);
-        public Task<byte[]> ExportStudentResults(ExportFileFormat format, long studentId, ExportByDateRangeModel exportModel);
-        public Task<byte[]> ExportStudentGroupResults(ExportFileFormat format, long courseId, ExportByDateRangeModel exportModel);
-        public Task<byte[]> ExportStudentsOfGroup(ExportFileFormat format, long groupId);
+        Task<byte[]> ExportStudentsClassbook(ExportFileFormat format, ExportByCourseAndStudentGroupModel exportModel);
+        Task<byte[]> ExportStudentsResults(ExportFileFormat format, ExportByCourseAndStudentGroupModel exportModel);
+        Task<byte[]> ExportStudentClassbook(ExportFileFormat format, long studentId, ExportByDateRangeModel exportModel);
+        Task<byte[]> ExportStudentResults(ExportFileFormat format, long studentId, ExportByDateRangeModel exportModel);
+        Task<byte[]> ExportStudentGroupResults(ExportFileFormat format, long courseId, ExportByDateRangeModel exportModel);
+        Task<byte[]> ExportStudentsOfGroup(ExportFileFormat format, long groupId);
     }
 }
