adaf5c7a2601faa53e80ef87ec7ff93179b25617
Babenko Serhii <sergebabenk@gmail.com>
2022-06-27 09:12:51 +0300

    added necessary classes

---
+++ CharlieBackend.Panel/Models/Languages/Languages.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CharlieBackend.Panel.Models.Languages
+{
+    public enum Language
+    {
+        [Description("en-US")]
+        EN,
+        [Description("uk-UA")]
+        UA
+    }
+
+    public static class Languages
+    {
+        public static Language language = Language.UA;
+    }
+
+    public static class LanguageExtensions
+    {
+        public static string ToDescriptionString(this Language val)
+        {
+            DescriptionAttribute[] attributes = (DescriptionAttribute[])val
+               .GetType()
+               .GetField(val.ToString())
+               .GetCustomAttributes(typeof(DescriptionAttribute), false);
+            return attributes.Length > 0 ? attributes[0].Description : string.Empty;
+        }
+    }
+}
